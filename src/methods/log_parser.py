2f83e64309fabda9dc1c707ce482b807fbd6e62b
unknown <justin_bunker@hotmail.com>
2019-11-22 12:37:32 +0000

    Update LogParser to get log message data from structured file path

--- src/methods/log_parser.py
+++ src/methods/log_parser.py
@@ -1,14 +1,13 @@
-from src.utils import read_file
+from src.utils import read_csv
 
 
 class LogParser:
     def __init__(self, log_file_path):
         self.cluster_templates = {}
         self.log_file_path = log_file_path
-        self.tokenized_log_entries = \
-            self._get_tokenized_log_entries_from_file_path(log_file_path)
+        self.tokenized_log_entries = self._get_tokenized_log_entries(log_file_path)
 
-    def _get_tokenized_log_entries_from_file_path(self, log_file_path):
-        raw_log = read_file(log_file_path)
-        log_lines = raw_log.split('\n')
-        return [log_line.split(' ') for log_line in log_lines]
+    def _get_tokenized_log_entries(self, structured_log_file_path):
+        raw_log = read_csv(structured_log_file_path)
+        result = [structured_log_line[-3].split(' ') for structured_log_line in raw_log[1:]]
+        return result
