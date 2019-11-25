c1be050980f37d5b9379b7cdb950a65e85f21dc6
unknown <justin_bunker@hotmail.com>
2019-11-25 01:06:05 +0000

    Add graphing scripts for comparing against the baseline on various datasets

---
+++ graphs/dataset_comparison_graph.py
@@ -0,0 +1,50 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from graphs.helpers import autolabel
+
+benchmark_baseline_accuracies = {
+    'Android': 0.712,
+    'Apache': 1.000,
+    'BGL': 0.939,
+    'Hadoop': 0.954,
+    'HDFS': 1.000,
+    'HealthApp': 0.822,
+    'HPC': 0.824,
+    'Linux': 0.672,
+    'Mac': 0.673,
+    'OpenSSH': 0.802,
+    'OpenStack': 0.871,
+    'Proxifier': 0.515,
+    'Spark': 0.920,
+    'Thunderbird': 0.663,
+    'Windows': 0.567,
+    'Zookeeper': 0.962,
+}
+
+final_best_accuracies = pickle.load(open('../results/dataset_comparison.p', 'rb'))
+print(final_best_accuracies)
+
+labels = benchmark_baseline_accuracies.keys()
+baseline_accuracies = benchmark_baseline_accuracies.values()
+candidate_accuracies = final_best_accuracies.values()
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width / 2, baseline_accuracies, width, label='Baseline')
+rects2 = ax.bar(x + width / 2, candidate_accuracies, width, label='Candidate')
+
+ax.set_ylabel('Accuracy')
+ax.set_title('IPLoM Accuracies')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+autolabel(ax, rects1)
+autolabel(ax, rects2)
+
+fig.tight_layout()
+
+plt.show()
---
+++ graphs/helpers.py
@@ -0,0 +1,11 @@
+def autolabel(ax, rects):
+    """
+    Attach a text label above each bar to display its height.
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
