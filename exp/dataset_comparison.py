4b969af39e1dd1e7af6b3c8892504f2db45041d1
unknown <justin_bunker@hotmail.com>
2019-11-25 00:10:25 +0000

    Use grid search in the dataset comparison experiment

--- exp/dataset_comparison.py
+++ exp/dataset_comparison.py
@@ -1,6 +1,7 @@
 from src.methods.iplom import Iplom
 from src.data_config import DataConfigs
 from src.helpers.evaluator import Evaluator
+from src.helpers.parameter_grid_searcher import ParameterGridSearcher
 
 data_set_configs = [
     DataConfigs.Android,
@@ -21,15 +22,22 @@ data_set_configs = [
     DataConfigs.Zookeeper,
 ]
 
+parameter_ranges_dict = {
+    'file_threshold': (0, 0.15, 0.05),
+    'partition_threshold': (0, 0.35, 0.05),
+    'lower_bound': (0.1, 0.35, 0.05),
+    'upper_bound': (0.9, 1, 1),
+    'goodness_threshold': (0.3, 0.65, 0.05)
+}
+
 for data_set_config in data_set_configs:
-    iplom = Iplom(data_set_config, **{
-        'file_threshold': 0.0,
-        'partition_threshold': 0.0,
-        'lower_bound': 0.1,
-        'upper_bound': 0.9,
-        'goodness_threshold': 0.35,
-    })
+    parameter_grid_searcher = ParameterGridSearcher(data_set_config, parameter_ranges_dict)
+    parameter_grid_searcher.search()
+
+    iplom = Iplom(data_set_config, **parameter_grid_searcher.best_parameters_dict)
     iplom.parse()
+
     evaluator = Evaluator(data_set_config, iplom.cluster_templates)
     iplom_bgl_accuracy = evaluator.evaluate()
+
     print('Final IPLoM {} Accuracy: {}'.format(data_set_config['name'], iplom_bgl_accuracy))
