2ea73bb3b7024468e659d404cfcb8d27af4c3e14
unknown <justin_bunker@hotmail.com>
2019-11-25 00:08:36 +0000

    Fix bug that caused infinite recursion in MappingFinder

--- src/helpers/mapping_finder.py
+++ src/helpers/mapping_finder.py
@@ -8,19 +8,21 @@ class MappingFinder:
         self.p1_token_mapping = deepcopy(p1_token_mapping)
         self.p2_token_mapping = deepcopy(p2_token_mapping)
 
-    def update_relevant_token_sets(self, token, parent_token=None, is_reversed=False):
-        if parent_token is None:
-            self.domain_set = set()
-            self.codomain_set = set()
+    def update_relevant_token_sets(self, token, is_reversed=False):
+        self.domain_set = set()
+        self.codomain_set = set()
+        self._update_relevant_token_sets(token, is_reversed)
+
+    def _update_relevant_token_sets(self, token, is_reversed):
         if not is_reversed:
             self.domain_set.add(token)
             for codomain_token in self.p1_token_mapping[token]:
-                if parent_token is None or parent_token != codomain_token:
+                if codomain_token not in self.codomain_set:
                     self.codomain_set.add(codomain_token)
-                    self.update_relevant_token_sets(codomain_token, token, not is_reversed)
+                    self._update_relevant_token_sets(codomain_token, not is_reversed)
         else:
             self.codomain_set.add(token)
             for domain_token in self.p2_token_mapping[token]:
-                if parent_token is None or parent_token != domain_token:
+                if domain_token not in self.domain_set:
                     self.domain_set.add(domain_token)
-                    self.update_relevant_token_sets(domain_token, token, not is_reversed)
+                    self._update_relevant_token_sets(domain_token, not is_reversed)
--- tests/helpers/mapping_finder_test.py
+++ tests/helpers/mapping_finder_test.py
@@ -1,7 +1,7 @@
 import unittest
 from src.helpers.mapping_finder import MappingFinder
 
-p1_token_mapping = {
+p1_token_mapping_1 = {
     'A': {'I'},
     'B': {'J', 'K'},
     'C': {'L'},
@@ -12,7 +12,7 @@ p1_token_mapping = {
     'H': {'P'},
 }
 
-p2_token_mapping = {
+p2_token_mapping_1 = {
     'I': {'A'},
     'J': {'B'},
     'K': {'B'},
@@ -23,7 +23,7 @@ p2_token_mapping = {
     'P': {'F', 'G', 'H'},
 }
 
-expected_results = {
+expected_results_1 = {
     'A': [{'A'}, {'I'}],
     'B': [{'B'}, {'J', 'K'}],
     'C': [{'C', 'D'}, {'L'}],
@@ -34,15 +34,38 @@ expected_results = {
     'H': [{'E', 'F', 'G', 'H'}, {'M', 'N', 'O', 'P'}],
 }
 
+p1_token_mapping_2 = {
+    'A': {'F', 'G'},
+    'B': {'G', 'F'},
+}
+
+p2_token_mapping_2 = {
+    'F': {'A', 'B'},
+    'G': {'B', 'A'},
+}
+
+expected_results_2 = {
+    'A': [{'A', 'B'}, {'F', 'G'}],
+    'B': [{'A', 'B'}, {'F', 'G'}],
+    'F': [{'A', 'B'}, {'F', 'G'}],
+    'G': [{'A', 'B'}, {'F', 'G'}],
+}
+
 
 class TestMappingFinder(unittest.TestCase):
-
     def test_A(self):
+        self._test(p1_token_mapping_1, p2_token_mapping_1, expected_results_1)
+
+    def test_B(self):
+        self._test(p1_token_mapping_2, p2_token_mapping_2, expected_results_2)
+
+    def _test(self, p1_token_mapping, p2_token_mapping, expected_results):
         mapping_finder = MappingFinder(p1_token_mapping, p2_token_mapping)
         for token in p1_token_mapping:
             mapping_finder.update_relevant_token_sets(token)
             domain_set = mapping_finder.domain_set
             codomain_set = mapping_finder.codomain_set
+
             domain_set_expected, codomain_set_expected = expected_results[token]
             self.assertEqual(domain_set, domain_set_expected)
             self.assertEqual(codomain_set, codomain_set_expected)
