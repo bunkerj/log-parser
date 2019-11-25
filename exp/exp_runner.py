e1bf0a63bed3c7e9812ecb22532c5d9518c75573
unknown <justin_bunker@hotmail.com>
2019-11-25 02:56:42 +0000

    Remove dead code from exp_runner

--- exp/exp_runner.py
+++ exp/exp_runner.py
@@ -1,21 +1,8 @@
 from src.methods.iplom import Iplom
 from src.data_config import DataConfigs
 from src.helpers.evaluator import Evaluator
-from src.helpers.parameter_grid_searcher import ParameterGridSearcher
 
-# parameter_ranges_dict = {
-#     'file_threshold': (0, 0.15, 0.05),
-#     'partition_threshold': (0, 0.35, 0.05),
-#     'lower_bound': (0.1, 0.35, 0.05),
-#     'upper_bound': (0.9, 1, 1),
-#     'goodness_threshold': (0.3, 0.65, 0.05)
-# }
-#
-# parameter_grid_searcher = ParameterGridSearcher(DataConfig.BGL, parameter_ranges_dict)
-# parameter_grid_searcher.search()
-# parameter_grid_searcher.print_results()
-
-iplom = Iplom(DataConfigs.BGL, **{
+iplom = Iplom(DataConfigs.OpenStack, **{
     'file_threshold': 0.0,
     'partition_threshold': 0.0,
     'lower_bound': 0.1,
@@ -23,9 +10,9 @@ iplom = Iplom(DataConfigs.BGL, **{
     'goodness_threshold': 0.35,
 })
 iplom.parse()
-# iplom.print_cluster_templates()
+iplom.print_cluster_templates()
 
-evaluator = Evaluator(DataConfigs.BGL, iplom.cluster_templates)
+evaluator = Evaluator(DataConfigs.OpenStack, iplom.cluster_templates)
 iplom_bgl_accuracy = evaluator.evaluate()
 
 print('Final IPLoM BGL Accuracy: {}'.format(iplom_bgl_accuracy))
