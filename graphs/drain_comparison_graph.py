41a8b8cf27016448be090a1b2b54c742019660f9
unknown <justin_bunker@hotmail.com>
2019-11-27 02:10:12 +0000

    Add Drain dataset comparison scripts

---
+++ graphs/drain_comparison_graph.py
@@ -0,0 +1,23 @@
+from graphs.utils import show_dataset_comparison_graph, load_results
+
+drain_benchmark_accuracies = {
+    'Android': 0.911,               # 0.911
+    'Apache': 1.000,                # 1.000
+    'BGL': 0.963,                   # 0.963
+    'Hadoop': 0.948,                # 0.948
+    'HDFS': 0.998,                  # 0.998
+    'HealthApp': 0.780,             # 0.780
+    'HPC': 0.887,                   # 0.887
+    'Linux': 0.690,                 # 0.690
+    'Mac': 0.787,                   # 0.787
+    'OpenSSH': 0.788,               # 0.788
+    'OpenStack': 0.733,             # 0.733
+    'Proxifier': 0.527,             # 0.527
+    'Spark': 0.920,                 # 0.920
+    'Thunderbird': 0.955,           # 0.955
+    'Windows': 0.997,               # 0.997
+    'Zookeeper': 0.967,             # 0.967
+}
+
+final_best_accuracies = load_results('drain_dataset_comparison.p')
+show_dataset_comparison_graph(drain_benchmark_accuracies, final_best_accuracies)
