b9196a40200d076027cd31c4e28aa0885d7b8a70
unknown <justin_bunker@hotmail.com>
2019-11-27 15:54:06 +0000

    Add parse() as an abstract method in LogParser

--- src/parsers/log_parser.py
+++ src/parsers/log_parser.py
@@ -1,15 +1,20 @@
 import re
 from src.utils import read_csv
+from abc import ABC, abstractmethod
 
 SPLIT_REGEX = r'[\s=:,]'
 
 
-class LogParser:
+class LogParser(ABC):
     def __init__(self, data_config):
         self.cluster_templates = {}
         self.regex_list = data_config['regex']
         self.log_file_path = data_config['path']
         self.tokenized_log_entries = self._get_tokenized_log_entries(data_config['path'])
+
+    @abstractmethod
+    def parse(self):
+        pass
 
     def _get_tokenized_log_entries(self, structured_log_file_path):
         raw_log = read_csv(structured_log_file_path)
