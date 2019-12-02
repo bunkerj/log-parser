d62fd06a795e4eb84ec62f605aebe715d35b0b85
unknown <justin_bunker@hotmail.com>
2019-12-02 22:13:21 +0000

    Update data_config paths

--- src/data_config.py
+++ src/data_config.py
@@ -1,120 +1,121 @@
 class DataConfigs:
     Android = {
         'name': 'Android',
-        'structured_path': '../../data/structured/Android_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Android_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Android_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Android_2k.log',
         'log_format': '<Date> <Time>  <Pid>  <Tid> <Level> <Component>: <Content>',
         'regex': [r'(/[\w-]+)+', r'([\w-]+\.){2,}[\w-]+', r'\b(\-?\+?\d+)\b|\b0[Xx][a-fA-F\d]+\b|\b[a-fA-F\d]{4,}\b'],
     }
     Apache = {
         'name': 'Apache',
-        'structured_path': '../../data/structured/Apache_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Apache_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Apache_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Apache_2k.log',
         'log_format': '\[<Time>\] \[<Level>\] <Content>',
         'regex': [r'(\d+\.){3}\d+'],
     }
     BGL = {
         'name': 'BGL',
-        'structured_path': '../../data/structured/BGL_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/BGL_2k.log',
+        'structured_path': '../../data/samples_2k/structured/BGL_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/BGL_2k.log',
         'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
         'regex': [r'core\.\d+'],
     }
     Hadoop = {
         'name': 'Hadoop',
-        'structured_path': '../../data/structured/Hadoop_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Hadoop_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Hadoop_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Hadoop_2k.log',
         'log_format': '<Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
     }
     HDFS = {
         'name': 'HDFS',
-        'structured_path': '../../data/structured/HDFS_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/HDFS_2k.log',
+        'structured_path': '../../data/samples_2k/structured/HDFS_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/HDFS_2k.log',
         'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
         'regex': [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'],
     }
     HealthApp = {
         'name': 'HealthApp',
-        'structured_path': '../../data/structured/HealthApp_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/HealthApp_2k.log',
+        'structured_path': '../../data/samples_2k/structured/HealthApp_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/HealthApp_2k.log',
         'log_format': '<Time>\|<Component>\|<Pid>\|<Content>',
         'regex': [],
     }
     HPC = {
         'name': 'HPC',
-        'structured_path': '../../data/structured/HPC_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/HPC_2k.log',
+        'structured_path': '../../data/samples_2k/structured/HPC_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/HPC_2k.log',
         'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
         'regex': [r'=\d+'],
     }
     Linux = {
         'name': 'Linux',
-        'structured_path': '../../data/structured/Linux_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Linux_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Linux_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Linux_2k.log',
         'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\d{2}:\d{2}:\d{2}'],
     }
     Mac = {
         'name': 'Mac',
-        'structured_path': '../../data/structured/Mac_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Mac_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Mac_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Mac_2k.log',
         'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
         'regex': [r'([\w-]+\.){2,}[\w-]+'],
     }
     OpenSSH = {
         'name': 'OpenSSH',
-        'structured_path': '../../data/structured/OpenSSH_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/OpenSSH_2k.log',
+        'structured_path': '../../data/samples_2k/structured/OpenSSH_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/OpenSSH_2k.log',
         'log_format': '<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'([\w-]+\.){2,}[\w-]+'],
     }
     OpenStack = {
         'name': 'OpenStack',
-        'structured_path': '../../data/structured/OpenStack_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/OpenStack_2k.log',
+        'structured_path': '../../data/samples_2k/structured/OpenStack_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/OpenStack_2k.log',
         'log_format': '<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>',
         'regex': [r'((\d+\.){3}\d+,?)+', r'/.+?\s', r'\d+'],
     }
     Proxifier = {
         'name': 'Proxifier',
-        'structured_path': '../../data/structured/Proxifier_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Proxifier_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Proxifier_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Proxifier_2k.log',
         'log_format': '\[<Time>\] <Program> - <Content>',
         'regex': [r'<\d+\ssec', r'([\w-]+\.)+[\w-]+(:\d+)?', r'\d{2}:\d{2}(:\d{2})*', r'[KGTM]B'],
     }
     Spark = {
         'name': 'Spark',
-        'structured_path': '../../data/structured/Spark_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Spark_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Spark_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Spark_2k.log',
         'log_format': '<Date> <Time> <Level> <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\b[KGTM]?B\b', r'([\w-]+\.){2,}[\w-]+'],
     }
     Thunderbird = {
         'name': 'Thunderbird',
-        'structured_path': '../../data/structured/Thunderbird_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Thunderbird_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Thunderbird_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Thunderbird_2k.log',
         'log_format': '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
     }
     Windows = {
         'name': 'Windows',
-        'structured_path': '../../data/structured/Windows_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Windows_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Windows_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Windows_2k.log',
         'log_format': '<Date> <Time>, <Level>                  <Component>    <Content>',
         'regex': [r'0x.*?\s'],
     }
     Zookeeper = {
         'name': 'Zookeeper',
-        'structured_path': '../../data/structured/Zookeeper_2k.log_structured.csv',
-        'unstructured_path': '../../data/unstructured/Zookeeper_2k.log',
+        'structured_path': '../../data/samples_2k/structured/Zookeeper_2k.log_structured.csv',
+        'unstructured_path': '../../data/samples_2k/unstructured/Zookeeper_2k.log',
         'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
         'regex': [r'(/|)(\d+\.){3}\d+(:\d+)?'],
     }
     BGL_FULL = {
         'name': 'BGL',
-        'structured_path': '../../data/structured/BGL_2k.log_structured.csv',
-        'unstructured_path': '../../data/full/BGL.log',
+        'unstructured_path': '../../data/full/unstructured/BGL.log',
+        'assignments_path': '../../data/full/assignments/BGL_assignments.txt',
+        'template_path': '../../data/full/templates/BGL_templates.csv',
         'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
         'regex': [r'core\.\d+'],
     }
