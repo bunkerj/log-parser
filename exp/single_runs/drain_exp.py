4025d132b6afb5cf289f5b017632a39840c0a57b
unknown <justin_bunker@hotmail.com>
2019-12-04 16:33:17 +0000

    Update existing single experiment script for IPLoM and add a separate script for Drain

---
+++ exp/single_runs/drain_exp.py
@@ -0,0 +1,13 @@
+from src.parsers.drain import Drain
+from src.data_config import DataConfigs
+from src.helpers.evaluator import Evaluator
+
+DATA_CONFIG = DataConfigs.BGL_FULL
+
+parser = Drain(DATA_CONFIG, 3, 100, 0.5)
+parser.parse()
+
+evaluator = Evaluator(DATA_CONFIG, parser.cluster_templates)
+iplom_bgl_accuracy = evaluator.evaluate()
+
+print('Final Drain Accuracy: {}'.format(iplom_bgl_accuracy))
---
+++ exp/single_runs/iplom_exp.py
@@ -0,0 +1,20 @@
+from src.parsers.iplom import Iplom
+from src.data_config import DataConfigs
+from src.helpers.evaluator import Evaluator
+
+DATA_CONFIG = DataConfigs.OpenStack
+
+parser = Iplom(DATA_CONFIG, **{
+    'file_threshold': 0.0,
+    'partition_threshold': 0.0,
+    'lower_bound': 0.1,
+    'upper_bound': 0.9,
+    'goodness_threshold': 0.35,
+})
+parser.parse()
+parser.print_cluster_templates()
+
+evaluator = Evaluator(DATA_CONFIG, parser.cluster_templates)
+iplom_accuracy = evaluator.evaluate()
+
+print('Final IPLoM Accuracy: {}'.format(iplom_accuracy))
