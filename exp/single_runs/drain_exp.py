268b83b8fbe2589757267f339b79dd19ef6b5e8f
unknown <justin_bunker@hotmail.com>
2019-12-06 16:45:10 +0000

    Change default dataset of drain_exp to BGL_FULL and also print timing information

--- exp/single_runs/drain_exp.py
+++ exp/single_runs/drain_exp.py
@@ -1,18 +1,27 @@
+from time import time
 from src.parsers.drain import Drain
 from src.data_config import DataConfigs
 from src.helpers.evaluator import Evaluator
 from src.helpers.data_manager import DataManager
 from src.utils import get_template_assignments
 
-DATA_CONFIG = DataConfigs.BGL
+DATA_CONFIG = DataConfigs.BGL_FULL
 
 data_manager = DataManager(DATA_CONFIG)
 tokenized_log_entries = data_manager.get_tokenized_log_entries()
-parser = Drain(tokenized_log_entries, 3, 100, 0.5)
-parser.parse()
-
 true_assignments = get_template_assignments(DATA_CONFIG['assignments_path'])
+parser = Drain(tokenized_log_entries, 3, 100, 0.5)
+
+start_time = time()
+parser.parse()
+minutes_to_parse = (time() - start_time) / 60
+
+start_time = time()
 evaluator = Evaluator(true_assignments, parser.cluster_templates)
 accuracy = evaluator.evaluate()
+minutes_to_evaluate = (time() - start_time) / 60
 
 print('Final Drain Accuracy: {}'.format(accuracy))
+print('Time to parse: {:.5f} minutes'.format(minutes_to_parse))
+print('Time to evaluate: {:.5f} minutes'.format(minutes_to_evaluate))
+print('Total time taken: {:.5f} minutes'.format(minutes_to_parse + minutes_to_evaluate))
