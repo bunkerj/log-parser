dbc111b1185af2049b67b98bf45588cd377752f7
unknown <justin_bunker@hotmail.com>
2019-12-06 00:57:40 +0000

    Add script for plotting drain tree depth over sensitivity

---
+++ exp/sensitivity_analysis/drain_tree_depth_over_sensitivity.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from src.utils import read_csv
+from src.parsers.drain import Drain
+from src.data_config import DataConfigs
+from src.helpers.evaluator import Evaluator
+from src.helpers.data_manager import DataManager
+
+DATA_CONFIG = DataConfigs.BGL
+
+accuracies = []
+tree_depths = list(range(3, 30, 1))
+true_assignments = read_csv(DATA_CONFIG['assignments_path'])
+
+for tree_depth in tree_depths:
+    data_manager = DataManager(DATA_CONFIG)
+    tokenized_log_entries = data_manager.get_tokenized_log_entries()
+    parser = Drain(tokenized_log_entries, 3, tree_depth, 0.5)
+    parser.parse()
+
+    evaluator = Evaluator(true_assignments, parser.cluster_templates)
+    accuracies.append(evaluator.evaluate())
+
+plt.plot(tree_depths, accuracies)
+plt.title('Drain Accuracy Over Tree Depth')
+plt.ylabel('Percentage Accuracy')
+plt.xlabel('Tree Depth')
+plt.grid()
+plt.show()
