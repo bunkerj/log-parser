2feb16b80ea62f4e8a750111167746ddc5ae2f15
unknown <justin_bunker@hotmail.com>
2020-01-28 04:08:52 +0000

    Add script to print out specified raw and tokenized log entries so that they can be easily compared and used for error analysis

---
+++ exp/single_runs/compare_raw_and_tokenized_log_entries.py
@@ -0,0 +1,11 @@
+"""
+Compare the raw and tokenized log entries for some specified indices.
+"""
+from src.data_config import DataConfigs
+from src.helpers.data_manager import DataManager
+
+DATA_CONFIG = DataConfigs.HPC
+INDICES = (401, 562, 699)
+
+data_manager = DataManager(DATA_CONFIG)
+data_manager.print_select_raw_and_tokenized_log_entries(INDICES)
