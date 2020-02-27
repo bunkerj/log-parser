aaa22c25965672be1a518daa17228d86c4674416
unknown <justin_bunker@hotmail.com>
2020-02-27 00:14:35 +0000

    Add the generate_dataset_properties_data script and update the corresponding utils file to create feature vectors and labels for all sample datasets

---
+++ exp/mixture_models/generate_dataset_properties_data.py
@@ -0,0 +1,75 @@
+"""
+The goal of this script is to create a labeled dataset to analyze the effect of
+providing labeled data to a clustering model.
+
+The predictors are:
+1) Vocabulary Size
+2) Average Token Frequency Entropy
+3) True Cluster Count
+4) Starting Impurity
+5) Provided labels Count
+
+The response variable is:
+Impurity Percentage Difference
+100 * (unlabeled impurity - labeled impurity) / (labeled impurity)
+"""
+from exp.mixture_models.utils import get_num_true_clusters, \
+    get_impurity_difference, normalize_matrix
+from exp.utils import dump_results
+from src.data_config import DataConfigs
+from src.helpers.data_manager import DataManager
+from exp.mixture_models.utils import get_avg_gini_impurity
+from graphs.utils import load_results
+from src.utils import get_template_assignments, get_vocabulary_indices, \
+    get_token_counts
+
+data_configs = [
+    DataConfigs.Android,
+    DataConfigs.Apache,
+    DataConfigs.BGL,
+    DataConfigs.Hadoop,
+    DataConfigs.HDFS,
+    DataConfigs.HealthApp,
+    DataConfigs.HPC,
+    DataConfigs.Linux,
+    DataConfigs.Mac,
+    DataConfigs.OpenSSH,
+    DataConfigs.OpenStack,
+    DataConfigs.Proxifier,
+    DataConfigs.Spark,
+    DataConfigs.Thunderbird,
+    DataConfigs.Windows,
+    DataConfigs.Zookeeper,
+]
+
+data = {}
+
+for data_config in data_configs:
+    name = data_config['name']
+    data_manager = DataManager(data_config)
+    tokenized_log_entries = data_manager.get_tokenized_no_num_log_entries()
+    true_assignments = get_template_assignments(data_config['assignments_path'])
+
+    v_indices = get_vocabulary_indices(tokenized_log_entries)
+    C = get_token_counts(tokenized_log_entries, v_indices)
+    C_probabilities = normalize_matrix(C, 1)
+    results = load_results(
+        'feedback_evaluation_mp_{}_5s.p'.format(name.lower()))
+
+    labeled_impurities = results['labeled_impurities']
+    unlabeled_impurities = results['unlabeled_impurities']
+    label_counts = results['label_counts']
+
+    print('{}: {}'.format(name, len(v_indices)))
+
+    data[name] = {}
+    data[name]['Vocabulary Size'] = len(v_indices)
+    data[name]['True Cluster Count'] = get_num_true_clusters(true_assignments)
+    data[name]['Starting Impurity'] = labeled_impurities[0]
+    data[name]['Provided labels Count'] = label_counts[2]
+    data[name]['Average Token Frequency Entropy'] = \
+        get_avg_gini_impurity(C_probabilities, 1)
+    data[name]['Impurity Percentage Difference'] = \
+        get_impurity_difference(labeled_impurities[2], unlabeled_impurities[2])
+
+dump_results('dataset_properties_data.p', data)
--- exp/mixture_models/utils.py
+++ exp/mixture_models/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from random import sample
 
 
@@ -14,3 +15,31 @@ def get_log_labels(true_assignments, num_of_labels):
 
 def get_num_true_clusters(true_assignments):
     return len(set(log_data[-1] for log_data in true_assignments))
+
+
+def normalize_vector(vector):
+    n = sum(vector)
+    if n == 0:
+        return vector
+    else:
+        return vector / n
+
+
+def normalize_matrix(matrix, axis):
+    return np.apply_along_axis(normalize_vector, axis, matrix)
+
+
+def get_gini_impurity(probabilities):
+    return sum(probabilities * (1 - probabilities))
+
+
+def get_avg_gini_impurity(probabilities_matrix, axis):
+    entropy_values = np.apply_along_axis(get_gini_impurity,
+                                         axis,
+                                         probabilities_matrix)
+    return sum(entropy_values) / len(entropy_values)
+
+
+def get_impurity_difference(labeled_impurity, unlabeled_impurity):
+    abs_diff = abs(labeled_impurity - unlabeled_impurity)
+    return 100 * abs_diff / labeled_impurity
