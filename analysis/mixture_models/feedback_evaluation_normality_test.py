f2fa3d778e0944425423a8fda86e134662379695
unknown <justin_bunker@hotmail.com>
2020-03-02 01:35:32 +0000

    Add script to test for normality

---
+++ analysis/mixture_models/feedback_evaluation_normality_test.py
@@ -0,0 +1,27 @@
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from graphs.utils import load_results
+from src.data_config import DataConfigs
+
+N_SAMPLES = 100
+LABEL_COUNT_IDX = 0
+DATA_CONFIG = DataConfigs.Apache
+
+dataset_name = DATA_CONFIG['name']
+results = load_results(
+    'feedback_evaluation_mp_filtered_no_num_{}_{}s.p'.format(
+        dataset_name.lower(), N_SAMPLES))
+
+label_counts = results['label_counts']
+unlab_samples = results['unlabeled_impurities_samples']
+unlab_sample_impurity_values = \
+    np.array([sample[LABEL_COUNT_IDX] for sample in unlab_samples])
+
+title = '{} Unlabeled Impurities with {} Labels and {} Samples' \
+    .format(dataset_name.capitalize(), label_counts[LABEL_COUNT_IDX], N_SAMPLES)
+
+sm.qqplot(unlab_sample_impurity_values, line='s')
+plt.title(title)
+plt.grid()
+plt.show()
