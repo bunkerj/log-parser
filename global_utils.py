4605e2ad02412934e33da9116a60f3f7677ef04d
unknown <justin_bunker@hotmail.com>
2020-04-14 11:24:09 -0400

    Add multi() to global_utils

--- global_utils.py
+++ global_utils.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+import numpy as np
+from scipy.special import gammaln
 
 from global_constants import RESULTS_DIR
 
@@ -14,3 +16,14 @@ def dump_results(name, results):
 def load_results(name):
     path = os.path.join(RESULTS_DIR, name)
     return pickle.load(open(path, 'rb'))
+
+
+def multi(x, params):
+    x_flat = x.flatten()
+    params_flat = params.flatten()
+    coeff = gammaln(x_flat.sum() + 1) - np.sum(gammaln(x_flat + 1))
+    valid_indices = np.nonzero(params_flat)
+    log_result = coeff + (x_flat[valid_indices] * np.log(
+        params_flat[valid_indices])).sum()
+    result = np.exp(log_result)
+    return result
