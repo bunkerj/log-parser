08a2e36db9219408788b7a92fea74086ea6c1a4d
unknown <justin_bunker@hotmail.com>
2020-04-14 15:57:04 -0400

    Change initialization shceme in online_vs_offline_em

--- exp/mixture_models/online_vs_offline_em.py
+++ exp/mixture_models/online_vs_offline_em.py
@@ -1,12 +1,15 @@
 """
 Log-likelihood comparison between online and offline EM.
 """
+from random import sample
 from copy import deepcopy
 from exp.mixture_models.utils import get_num_true_clusters
 from global_utils import dump_results
 from src.data_config import DataConfigs
 from src.helpers.data_manager import DataManager
 from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
+
+N_INIT = 100
 
 data_configs = [
     DataConfigs.Android,
@@ -40,10 +43,17 @@ for data_config in data_configs:
     true_assignments = data_manager.get_true_assignments()
     n_true_clusters = get_num_true_clusters(true_assignments)
 
-    offline_em_parser = MultinomialMixtureOnline(log_entries,
-                                                 n_true_clusters,
-                                                 False)
-    online_em_parser = deepcopy(offline_em_parser)
+    init_indices = sample(range(len(log_entries)), k=N_INIT)
+    init_log_entries = [log_entries[idx] for idx in init_indices]
+
+    online_em_parser = MultinomialMixtureOnline(log_entries,
+                                                n_true_clusters,
+                                                False,
+                                                epsilon=0.01)
+    online_em_parser.find_best_initialization(init_log_entries)
+
+    offline_em_parser = deepcopy(online_em_parser)
+    offline_em_parser.init_sufficient_stats()
 
     offline_em_parser.perform_offline_em(log_entries, track_history=True)
     offline_ll_history = offline_em_parser.get_log_likelihood_history()
@@ -51,7 +61,7 @@ for data_config in data_configs:
 
     online_em_parser.perform_online_batch_em(log_entries,
                                              len(offline_ll_history) - 1,
-                                             True)
+                                             track_history=True)
     online_ll_history = online_em_parser.get_log_likelihood_history()
     results[name]['online'] = online_ll_history
 
