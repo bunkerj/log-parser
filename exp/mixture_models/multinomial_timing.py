0a2a8c04a19f137089cc81d9f21b53f41d89ab62
unknown <justin_bunker@hotmail.com>
2020-04-15 01:45:48 -0400

    Fix timing variables in multinomial_timing

--- exp/mixture_models/multinomial_timing.py
+++ exp/mixture_models/multinomial_timing.py
@@ -12,15 +12,15 @@ samples = np.zeros((N_SAMPLES, N_CLASSES))
 for idx, params in enumerate(total_params):
     samples[idx, :] = np.random.multinomial(N_EXPERIMENTS, params)
 
-scipy_multi_time = time()
-for idx in range(N_SAMPLES):
-    result = multi_custom_pmf(samples[idx, :], total_params[idx, :])
-scipy_multi_time = time() - scipy_multi_time
-
 custom_multi_time = time()
 for idx in range(N_SAMPLES):
-    multi_scipy.pmf(samples[idx, :], sum(samples[idx, :]), total_params[idx, :])
+    result = multi_custom_pmf(samples[idx, :], total_params[idx, :])
 custom_multi_time = time() - custom_multi_time
+
+scipy_multi_time = time()
+for idx in range(N_SAMPLES):
+    multi_scipy.pmf(samples[idx, :], sum(samples[idx, :]), total_params[idx, :])
+scipy_multi_time = time() - scipy_multi_time
 
 print('Scipy implementation: {}'.format(scipy_multi_time))
 print('Custom implementation: {}'.format(custom_multi_time))
