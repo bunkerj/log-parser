7606e78432b49c8c062329e7ecf53957a214af0b
unknown <justin_bunker@hotmail.com>
2020-04-16 13:30:45 -0400

    Update the online_em_script to run for multiple sample iterations

--- exp/mixture_models/online_em.py
+++ exp/mixture_models/online_em.py
@@ -13,8 +13,9 @@ from src.helpers.data_manager import DataManager
 from src.helpers.evaluator import Evaluator
 from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
 
+N_SAMPLE = 25
 DATA_CONFIG = DataConfigs.Apache
-N_SAMPLE_SIZES = list(np.linspace(50, 2000, 40, dtype=np.int))
+INIT_SIZES = list(np.linspace(50, 2000, 40, dtype=np.int))
 
 # Get relevant data
 data_manager = DataManager(DATA_CONFIG)
@@ -22,54 +23,69 @@ log_entries = data_manager.get_tokenized_no_num_log_entries()
 true_assignments = data_manager.get_true_assignments()
 n_true_clusters = get_num_true_clusters(true_assignments)
 
-cem_parser = MultinomialMixtureOnline(log_entries, n_true_clusters, True)
+cem_parser = MultinomialMixtureOnline(log_entries, n_true_clusters,
+                                      False, alpha=1.05, beta=1.05)
 
-online_cem_parser = MultinomialMixtureOnline(log_entries, n_true_clusters, True)
+online_cem_parser = MultinomialMixtureOnline(log_entries, n_true_clusters,
+                                             True, alpha=1.05, beta=1.05)
 online_cem_parser.set_parameters(cem_parser.get_parameters())
 
-online_em_parser = MultinomialMixtureOnline(log_entries, n_true_clusters, False)
+online_em_parser = MultinomialMixtureOnline(log_entries, n_true_clusters,
+                                            False, alpha=1.05, beta=1.05)
 online_em_parser.set_parameters(cem_parser.get_parameters())
 
 results = {
-    'sample_sizes': N_SAMPLE_SIZES,
+    'init_sizes': INIT_SIZES,
     'scores': {'cem': [], 'online_cem': [], 'online_em': []},
     'timings': {'cem': [], 'online_cem': [], 'online_em': []},
 }
 
-for sample_size in N_SAMPLE_SIZES:
-    print('Sample size: {}...'.format(sample_size))
+for init_size in INIT_SIZES:
+    cem_score = 0
+    online_cem_score = 0
+    online_em_score = 0
 
-    # Randomly sample data based on sample_size
-    sample_indices = sample(range(len(log_entries)), k=sample_size)
-    sample_log_entries = [log_entries[idx] for idx in sample_indices]
+    cem_timing = 0
+    online_cem_timing = 0
+    online_em_timing = 0
 
-    # Fit parameters on all data points within the sample
-    sample_cem_parser = deepcopy(cem_parser)
-    sample_online_cem_parser = deepcopy(online_cem_parser)
-    sample_online_em_parser = deepcopy(online_em_parser)
+    for _ in range(N_SAMPLE):
+        print('Sample size: {}...'.format(init_size))
 
-    cem_timing = time()
-    sample_cem_parser.perform_offline_em(sample_log_entries)
-    cem_timing = time() - cem_timing
+        # Randomly sample data based on sample_size
+        sample_indices = sample(range(len(log_entries)), k=init_size)
+        sample_log_entries = [log_entries[idx] for idx in sample_indices]
 
-    online_cem_timing = time()
-    sample_online_cem_parser.perform_online_batch_em(sample_log_entries)
-    online_cem_timing = time() - online_cem_timing
+        # Fit parameters on all data points within the sample
+        sample_cem_parser = deepcopy(cem_parser)
+        sample_online_cem_parser = deepcopy(online_cem_parser)
+        sample_online_em_parser = deepcopy(online_em_parser)
 
-    online_em_timing = time()
-    sample_online_em_parser.perform_online_batch_em(sample_log_entries)
-    online_em_timing = time() - online_em_timing
+        cem_timing_tmp = time()
+        sample_cem_parser.perform_offline_em(sample_log_entries)
+        cem_timing += (time() - cem_timing_tmp) / N_SAMPLE
 
-    # Perform accuracy evaluations
-    evaluator = Evaluator(true_assignments)
+        online_cem_timing_tmp = time()
+        sample_online_cem_parser.perform_online_batch_em(sample_log_entries)
+        online_cem_timing += (time() - online_cem_timing_tmp) / N_SAMPLE
 
-    cem_clusters = sample_cem_parser.get_clusters(log_entries)
-    online_cem_clusters = sample_online_cem_parser.get_clusters(log_entries)
-    online_em_clusters = sample_online_em_parser.get_clusters(log_entries)
+        online_em_timing_tmp = time()
+        sample_online_em_parser.perform_online_batch_em(sample_log_entries)
+        online_em_timing += (time() - online_em_timing_tmp) / N_SAMPLE
 
-    cem_score = evaluator.get_impurity(cem_clusters, [])
-    online_cem_score = evaluator.get_impurity(online_cem_clusters, [])
-    online_em_score = evaluator.get_impurity(online_em_clusters, [])
+        # Perform accuracy evaluations
+        evaluator = Evaluator(true_assignments)
+
+        cem_clusters = sample_cem_parser.get_clusters(log_entries)
+        online_cem_clusters = sample_online_cem_parser.get_clusters(log_entries)
+        online_em_clusters = sample_online_em_parser.get_clusters(log_entries)
+
+        cem_score += evaluator.get_impurity(cem_clusters,
+                                            []) / N_SAMPLE
+        online_cem_score += evaluator.get_impurity(online_cem_clusters,
+                                                   []) / N_SAMPLE
+        online_em_score += evaluator.get_impurity(online_em_clusters,
+                                                  []) / N_SAMPLE
 
     # Record accuracies
     results_acc = results['scores']
--- graphs/mixture_models/online_em_graph.py
+++ graphs/mixture_models/online_em_graph.py
@@ -3,23 +3,23 @@ from global_utils import load_results
 
 results = load_results('online_em_results.p')
 
-sample_sizes = results['sample_sizes']
+init_sizes = results['init_sizes']
 results_scores = results['scores']
 results_timings = results['timings']
 
 plt.subplot(1, 2, 1)
-plt.plot(sample_sizes, results_timings['cem'])
-plt.plot(sample_sizes, results_timings['online_cem'])
-plt.plot(sample_sizes, results_timings['online_em'])
+plt.plot(init_sizes, results_timings['cem'])
+plt.plot(init_sizes, results_timings['online_cem'])
+plt.plot(init_sizes, results_timings['online_em'])
 plt.legend(['CEM', 'Online CEM', 'Online EM'])
 plt.ylabel('Timing (Seconds)')
 plt.xlabel('Sample Size')
 plt.grid()
 
 plt.subplot(1, 2, 2)
-plt.plot(sample_sizes, results_scores['cem'])
-plt.plot(sample_sizes, results_scores['online_cem'])
-plt.plot(sample_sizes, results_scores['online_em'])
+plt.plot(init_sizes, results_scores['cem'])
+plt.plot(init_sizes, results_scores['online_cem'])
+plt.plot(init_sizes, results_scores['online_em'])
 plt.legend(['CEM', 'Online CEM', 'Online EM'])
 plt.ylabel('Impurity')
 plt.xlabel('Sample Size')
