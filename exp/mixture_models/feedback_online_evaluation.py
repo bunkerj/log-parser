a7238243bf118348e35ddc8eaf0541f702fadb7e
unknown <justin_bunker@hotmail.com>
2020-04-16 22:57:42 -0400

    Update feedback_online_evaluation to use to reflect changes from MultinomialMixtureOnline

--- exp/mixture_models/feedback_online_evaluation.py
+++ exp/mixture_models/feedback_online_evaluation.py
@@ -1,19 +1,17 @@
 """
-********************************* DEPRECATED *********************************
-[Only used as a performance benchmark against feedback_evaluation_mp]
-******************************************************************************
 Evaluate how different initializations have an impact on impurity when using
-the multinomial mixture model.
+the online multinomial mixture model.
 """
 from time import time
 from exp.mixture_models.utils import get_log_labels, get_num_true_clusters
 from global_utils import dump_results
-from src.parsers.multinomial_mixture import MultinomialMixture
 from src.data_config import DataConfigs
 from src.helpers.evaluator import Evaluator
 from src.helpers.data_manager import DataManager
+from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
 
-N_SAMPLES = 10
+N_INIT = 50
+N_SAMPLES = 3
 DATA_CONFIG = DataConfigs.Apache
 LABEL_COUNTS = [0, 200, 400, 600, 800, 1000]
 
@@ -24,41 +22,70 @@ num_true_clusters = get_num_true_clusters(true_assignments)
 evaluator = Evaluator(true_assignments)
 
 results = {
-    'labeled_impurities': [],
-    'unlabeled_impurities': [],
+    'n_logs': len(tokenized_log_entries),
+    'avg_labeled_impurities': [],
+    'avg_unlabeled_impurities': [],
+    'labeled_impurities_samples': [],
+    'unlabeled_impurities_samples': [],
     'label_counts': LABEL_COUNTS,
 }
+lab_impurities_samples = []
+unlab_impurities_samples = []
 
 start = time()
 
-for num_label in LABEL_COUNTS:
-    print('Processing for {}...'.format(num_label))
-    lab_impurity = 0
-    unlab_impurity = 0
-    for i in range(N_SAMPLES):
-        lab_parser = MultinomialMixture(tokenized_log_entries,
-                                        num_true_clusters)
-        unlab_parser = MultinomialMixture(tokenized_log_entries,
-                                          num_true_clusters)
-        unlab_parser.initialize_responsibilities(lab_parser)
+for sample_idx in range(N_SAMPLES):
+    print('Sample {}...'.format(sample_idx))
+    lab_impurities_samples.append([])
+    unlab_impurities_samples.append([])
 
-        log_labels = get_log_labels(true_assignments, num_label)
-        lab_parser.label_logs(log_labels)
+    for label_count in LABEL_COUNTS:
+        print(label_count)
+        lab_parser = MultinomialMixtureOnline(tokenized_log_entries,
+                                              num_true_clusters,
+                                              is_classification=False,
+                                              alpha=1.05,
+                                              beta=1.05)
+        unlab_parser = MultinomialMixtureOnline(tokenized_log_entries,
+                                                num_true_clusters,
+                                                is_classification=False,
+                                                alpha=1.05,
+                                                beta=1.05)
+        unlab_parser.set_parameters(lab_parser.get_parameters())
+
+        log_labels = get_log_labels(true_assignments, label_count)
+        lab_parser.label_logs(log_labels, tokenized_log_entries)
         labeled_indices = lab_parser.labeled_indices
 
-        lab_parser.parse()
-        lab_impurity += evaluator.get_impurity(lab_parser.cluster_templates,
-                                               labeled_indices) / N_SAMPLES
-        unlab_parser.parse()
-        unlab_impurity += evaluator.get_impurity(unlab_parser.cluster_templates,
-                                                 labeled_indices) / N_SAMPLES
+        lab_parser.perform_online_batch_em(tokenized_log_entries)
+        lab_cluster_templates = lab_parser.get_clusters(tokenized_log_entries)
+        lab_impurity = evaluator.get_impurity(lab_cluster_templates,
+                                              labeled_indices)
 
-    results['labeled_impurities'].append(lab_impurity)
-    results['unlabeled_impurities'].append(unlab_impurity)
+        unlab_parser.perform_online_batch_em(tokenized_log_entries)
+        unlab_cluster_templates = unlab_parser.get_clusters(
+            tokenized_log_entries)
+        unlab_impurity = evaluator.get_impurity(unlab_cluster_templates,
+                                                labeled_indices)
+
+        lab_impurities_samples[sample_idx].append(lab_impurity)
+        unlab_impurities_samples[sample_idx].append(unlab_impurity)
 
 print('\nTime taken: {}\n'.format(time() - start))
 
-result_filename = 'feedback_evaluation_{}_{}s.p'.format(
+for label_idx in range(len(LABEL_COUNTS)):
+    avg_lab_impurity = 0
+    avg_unlab_impurity = 0
+    for sample_idx in range(N_SAMPLES):
+        avg_lab_impurity += lab_impurities_samples[sample_idx][label_idx]
+        avg_unlab_impurity += unlab_impurities_samples[sample_idx][label_idx]
+    results['avg_labeled_impurities'].append(avg_lab_impurity / N_SAMPLES)
+    results['avg_unlabeled_impurities'].append(avg_unlab_impurity / N_SAMPLES)
+
+results['labeled_impurities_samples'] = lab_impurities_samples
+results['unlabeled_impurities_samples'] = unlab_impurities_samples
+
+result_filename = 'feedback_online_evaluation_{}_{}s.p'.format(
     DATA_CONFIG['name'].lower(), N_SAMPLES)
 dump_results(result_filename, results)
 
