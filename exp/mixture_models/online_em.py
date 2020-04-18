7df7ca4850f5f68d432daea89c1af2fadeea5e2d
unknown <justin_bunker@hotmail.com>
2020-04-18 18:15:35 -0400

    Fix naming of of cem to em and add cem (offline)

--- exp/mixture_models/online_em.py
+++ exp/mixture_models/online_em.py
@@ -1,5 +1,5 @@
 """
-Evaluate and compare the timings and scores between CEM, Online EM, and
+Evaluate and compare the timings and scores between EM, CEM, Online EM, and
 Online CEM.
 """
 import numpy as np
@@ -13,8 +13,8 @@ from src.helpers.evaluator import Evaluator
 from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
 
 N_SAMPLE = 20
-DATA_CONFIG = DataConfigs.Apache
-TRAINING_SIZES = list(np.linspace(50, 2000, 40, dtype=np.int))
+DATA_CONFIG = DataConfigs.HPC
+TRAINING_SIZES = list(np.linspace(30, 2000, 20, dtype=np.int))
 
 # Get relevant data
 data_manager = DataManager(DATA_CONFIG)
@@ -24,18 +24,20 @@ n_true_clusters = get_num_true_clusters(true_assignments)
 
 results = {
     'training_sizes': TRAINING_SIZES,
-    'scores': {'cem': [], 'online_cem': [], 'online_em': []},
-    'timings': {'cem': [], 'online_cem': [], 'online_em': []},
+    'scores': {'em': [], 'cem': [], 'online_em': [], 'online_cem': []},
+    'timings': {'em': [], 'cem': [], 'online_em': [], 'online_cem': []},
 }
 
 for training_size in TRAINING_SIZES:
+    em_score = 0
     cem_score = 0
-    online_cem_score = 0
     online_em_score = 0
+    online_cem_score = 0
 
+    em_timing = 0
     cem_timing = 0
-    online_cem_timing = 0
     online_em_timing = 0
+    online_cem_timing = 0
 
     for _ in range(N_SAMPLE):
         print('Sample size: {}...'.format(training_size))
@@ -45,17 +47,17 @@ for training_size in TRAINING_SIZES:
         training_log_entries = [log_entries[idx] for idx in training_indices]
 
         # Fit parameters on all training logs
+        em_parser = MultinomialMixtureOnline(log_entries,
+                                             n_true_clusters,
+                                             is_classification=False,
+                                             alpha=1.05,
+                                             beta=1.05)
+
         cem_parser = MultinomialMixtureOnline(log_entries,
                                               n_true_clusters,
-                                              is_classification=False,
+                                              is_classification=True,
                                               alpha=1.05,
                                               beta=1.05)
-
-        online_cem_parser = MultinomialMixtureOnline(log_entries,
-                                                     n_true_clusters,
-                                                     is_classification=True,
-                                                     alpha=1.05,
-                                                     beta=1.05)
 
         online_em_parser = MultinomialMixtureOnline(log_entries,
                                                     n_true_clusters,
@@ -63,47 +65,59 @@ for training_size in TRAINING_SIZES:
                                                     alpha=1.05,
                                                     beta=1.05)
 
-        online_cem_parser.set_parameters(cem_parser.get_parameters())
-        online_em_parser.set_parameters(cem_parser.get_parameters())
+        online_cem_parser = MultinomialMixtureOnline(log_entries,
+                                                     n_true_clusters,
+                                                     is_classification=True,
+                                                     alpha=1.05,
+                                                     beta=1.05)
+
+        cem_parser.set_parameters(em_parser.get_parameters())
+        online_cem_parser.set_parameters(em_parser.get_parameters())
+        online_em_parser.set_parameters(em_parser.get_parameters())
+
+        em_timing_tmp = time()
+        em_parser.perform_offline_em(training_log_entries)
+        em_timing += (time() - em_timing_tmp) / N_SAMPLE
 
         cem_timing_tmp = time()
         cem_parser.perform_offline_em(training_log_entries)
         cem_timing += (time() - cem_timing_tmp) / N_SAMPLE
 
-        online_cem_timing_tmp = time()
-        online_cem_parser.perform_online_batch_em(training_log_entries)
-        online_cem_timing += (time() - online_cem_timing_tmp) / N_SAMPLE
-
         online_em_timing_tmp = time()
         online_em_parser.perform_online_batch_em(training_log_entries)
         online_em_timing += (time() - online_em_timing_tmp) / N_SAMPLE
 
+        online_cem_timing_tmp = time()
+        online_cem_parser.perform_online_batch_em(training_log_entries)
+        online_cem_timing += (time() - online_cem_timing_tmp) / N_SAMPLE
+
         # Perform accuracy evaluations
-        evaluator = Evaluator(true_assignments)
+        ev = Evaluator(true_assignments)
 
+        em_clusters = em_parser.get_clusters(log_entries)
         cem_clusters = cem_parser.get_clusters(log_entries)
-        online_cem_clusters = online_cem_parser.get_clusters(log_entries)
         online_em_clusters = online_em_parser.get_clusters(log_entries)
+        online_cem_clusters = online_cem_parser.get_clusters(log_entries)
 
-        cem_score += evaluator.get_impurity(cem_clusters,
-                                            []) / N_SAMPLE
-        online_cem_score += evaluator.get_impurity(online_cem_clusters,
-                                                   []) / N_SAMPLE
-        online_em_score += evaluator.get_impurity(online_em_clusters,
-                                                  []) / N_SAMPLE
+        em_score += ev.get_impurity(em_clusters, []) / N_SAMPLE
+        cem_score += ev.get_impurity(cem_clusters, []) / N_SAMPLE
+        online_em_score += ev.get_impurity(online_em_clusters, []) / N_SAMPLE
+        online_cem_score += ev.get_impurity(online_cem_clusters, []) / N_SAMPLE
 
     # Record scores
     results_acc = results['scores']
+    results_acc['em'].append(em_score)
     results_acc['cem'].append(cem_score)
-    results_acc['online_cem'].append(online_cem_score)
     results_acc['online_em'].append(online_em_score)
+    results_acc['online_cem'].append(online_cem_score)
 
     # Record timings
     results_tim = results['timings']
+    results_tim['em'].append(em_timing)
     results_tim['cem'].append(cem_timing)
-    results_tim['online_cem'].append(online_cem_timing)
     results_tim['online_em'].append(online_em_timing)
+    results_tim['online_cem'].append(online_cem_timing)
 
-dump_results('online_em_results.p', results)
+dump_results('online_em_results2_hpc_20s.p', results)
 
 print('done!')
