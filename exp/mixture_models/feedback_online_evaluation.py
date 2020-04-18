6f5b8643c602fddf43ccf59593f59118133ebce8
unknown <justin_bunker@hotmail.com>
2020-04-18 18:14:31 -0400

    Keep track of average timings in feedback_online_evaluation

--- exp/mixture_models/feedback_online_evaluation.py
+++ exp/mixture_models/feedback_online_evaluation.py
@@ -11,26 +11,30 @@ from src.helpers.data_manager import DataManager
 from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
 
 N_INIT = 50
-N_SAMPLES = 3
-DATA_CONFIG = DataConfigs.Apache
+N_SAMPLES = 20
+DATA_CONFIG = DataConfigs.HPC
 LABEL_COUNTS = [0, 200, 400, 600, 800, 1000]
 
 data_manager = DataManager(DATA_CONFIG)
 tokenized_log_entries = data_manager.get_tokenized_no_num_log_entries()
 true_assignments = data_manager.get_true_assignments()
 num_true_clusters = get_num_true_clusters(true_assignments)
-evaluator = Evaluator(true_assignments)
+ev = Evaluator(true_assignments)
 
 results = {
     'n_logs': len(tokenized_log_entries),
     'avg_labeled_impurities': [],
     'avg_unlabeled_impurities': [],
+    'avg_labeled_timing': 0,
+    'avg_unlabeled_timing': 0,
     'labeled_impurities_samples': [],
     'unlabeled_impurities_samples': [],
     'label_counts': LABEL_COUNTS,
 }
 lab_impurities_samples = []
 unlab_impurities_samples = []
+
+print(DATA_CONFIG['name'])
 
 start = time()
 
@@ -57,16 +61,19 @@ for sample_idx in range(N_SAMPLES):
         lab_parser.label_logs(log_labels, tokenized_log_entries)
         labeled_indices = lab_parser.labeled_indices
 
+        lab_timing = time()
         lab_parser.perform_online_batch_em(tokenized_log_entries)
-        lab_cluster_templates = lab_parser.get_clusters(tokenized_log_entries)
-        lab_impurity = evaluator.get_impurity(lab_cluster_templates,
-                                              labeled_indices)
+        results['avg_labeled_timing'] += (time() - lab_timing) / N_SAMPLES
 
+        lab_cluster_templ = lab_parser.get_clusters(tokenized_log_entries)
+        lab_impurity = ev.get_impurity(lab_cluster_templ, labeled_indices)
+
+        unlab_timing = time()
         unlab_parser.perform_online_batch_em(tokenized_log_entries)
-        unlab_cluster_templates = unlab_parser.get_clusters(
-            tokenized_log_entries)
-        unlab_impurity = evaluator.get_impurity(unlab_cluster_templates,
-                                                labeled_indices)
+        results['avg_unlabeled_timing'] += (time() - unlab_timing) / N_SAMPLES
+
+        unlab_cluster_templ = unlab_parser.get_clusters(tokenized_log_entries)
+        unlab_impurity = ev.get_impurity(unlab_cluster_templ, labeled_indices)
 
         lab_impurities_samples[sample_idx].append(lab_impurity)
         unlab_impurities_samples[sample_idx].append(unlab_impurity)
