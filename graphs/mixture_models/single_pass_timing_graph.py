c6a05151a2c4f165d98740dc1af1398e28f4f1f8
unknown <justin_bunker@hotmail.com>
2020-05-03 00:45:00 -0400

    Refactor single_pass_timing_graph

--- graphs/mixture_models/single_pass_timing_graph.py
+++ graphs/mixture_models/single_pass_timing_graph.py
@@ -4,6 +4,6 @@ PADDING = 20
 
 results = load_results('single_pass_timing.p')
 
-print('Name:'.ljust(PADDING) + '{}'.format(results['name']))
-print('Clusters:'.ljust(PADDING) + '{}'.format(results['n_clusters']))
-print('Timing:'.ljust(PADDING) + '{:.8}'.format(results['timing']))
+print('{:<15}{}'.format('Name:', results['name']))
+print('{:<15}{}'.format('Clusters:', results['n_clusters']))
+print('{:<15}{:.8}'.format('Timing:', results['timing']))
