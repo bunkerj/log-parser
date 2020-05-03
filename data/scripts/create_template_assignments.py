0c82a229e81d5b141e49b644818b1f390f7867c9
unknown <justin_bunker@hotmail.com>
2020-05-03 19:59:36 -0400

    Update create_template_assignments so that it can handle multiple configs

--- data/scripts/create_template_assignments.py
+++ data/scripts/create_template_assignments.py
@@ -4,5 +4,23 @@ Creates the template assignment for a specific dataset configuration.
 from src.helpers.template_assigner import TemplateAssigner
 from src.data_config import DataConfigs
 
-template_assigner = TemplateAssigner(DataConfigs.BGL_FULL)
-template_assigner.write_assignments()
+data_configs = [
+    DataConfigs.Android_FULL,
+    DataConfigs.Apache_FULL,
+    DataConfigs.BGL_FULL,
+    DataConfigs.Hadoop_FULL,
+    DataConfigs.HDFS_FULL,
+    DataConfigs.HealthApp_FULL,
+    DataConfigs.HPC_FULL,
+    DataConfigs.Linux_FULL,
+    DataConfigs.Mac_FULL,
+    DataConfigs.OpenSSH_FULL,
+    DataConfigs.OpenStack_FULL,
+    DataConfigs.Proxifier_FULL,
+    DataConfigs.Spark_FULL,
+    DataConfigs.Zookeeper_FULL,
+]
+
+for data_config in data_configs:
+    template_assigner = TemplateAssigner(data_config)
+    template_assigner.write_assignments()
