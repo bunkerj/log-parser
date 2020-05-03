06ab2537453ac9ac2a6459730b2a8c84f382d9dd
unknown <justin_bunker@hotmail.com>
2020-05-03 19:57:40 -0400

    Add online_benchmark experiment

---
+++ exp/mixture_models/online_benchmark.py
@@ -0,0 +1,61 @@
+"""
+Compare online mixture model with random initialization against online mixture
+model with Drain initialization.
+"""
+from global_utils import dump_results
+from src.data_config import DataConfigs
+from src.helpers.evaluator import Evaluator
+from src.helpers.data_manager import DataManager
+from exp.mixture_models.utils import get_num_true_clusters, get_log_labels
+from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
+
+
+def run_online_benchmark(n_labels):
+    data_configs = [
+        DataConfigs.Android,
+        DataConfigs.Apache,
+        DataConfigs.BGL,
+        DataConfigs.Hadoop,
+        DataConfigs.HDFS,
+        DataConfigs.HealthApp,
+        DataConfigs.HPC,
+        DataConfigs.Linux,
+        DataConfigs.Mac,
+        DataConfigs.OpenSSH,
+        DataConfigs.OpenStack,
+        DataConfigs.Proxifier,
+        DataConfigs.Spark,
+        DataConfigs.Thunderbird,
+        DataConfigs.Windows,
+        DataConfigs.Zookeeper,
+    ]
+
+    results = {}
+    for data_config in data_configs:
+        data_manager = DataManager(data_config)
+        logs = data_manager.get_tokenized_no_num_logs()
+        true_assignments = data_manager.get_true_assignments()
+        ev = Evaluator(true_assignments)
+
+        n_true_clusters = get_num_true_clusters(true_assignments)
+        log_labels = get_log_labels(true_assignments, n_labels)
+
+        parser = MultinomialMixtureOnline(logs,
+                                          n_true_clusters,
+                                          is_classification=False,
+                                          alpha=1.05,
+                                          beta=1.05)
+
+        parser.label_logs(log_labels, logs)
+
+        clusters = parser.get_clusters(logs)
+        results[data_config['name']] = ev.evaluate(clusters)
+
+    return results
+
+
+if __name__ == '__main__':
+    n_labels = 50
+
+    results = run_online_benchmark(n_labels)
+    dump_results('online_benchmark.p', results)
---
+++ graphs/mixture_models/online_benchmark_graph.py
@@ -0,0 +1,36 @@
+"""
+Plots a histogram of Drain experiments (from the dataset_comparison directory)
+to compare the accuracies against the ones acquired from the LogPAI open source
+benchmark.
+
+In the comments, are the results as reported by the associated benchmark paper
+from Zhu et al., 2019.
+"""
+
+from graphs.utils import plot_dataset_comparison_graph
+from global_utils import load_results
+
+drain_benchmark_accuracies = {      # Results from benchmark paper
+    'Android': 0.911,               # 0.911
+    'Apache': 1.000,                # 1.000
+    'BGL': 0.963,                   # 0.963
+    'Hadoop': 0.948,                # 0.948
+    'HDFS': 0.998,                  # 0.998
+    'HealthApp': 0.780,             # 0.780
+    'HPC': 0.887,                   # 0.887
+    'Linux': 0.690,                 # 0.690
+    'Mac': 0.787,                   # 0.787
+    'OpenSSH': 0.788,               # 0.788
+    'OpenStack': 0.733,             # 0.733
+    'Proxifier': 0.527,             # 0.527
+    'Spark': 0.920,                 # 0.920
+    'Thunderbird': 0.955,           # 0.955
+    'Windows': 0.997,               # 0.997
+    'Zookeeper': 0.967,             # 0.967
+}
+
+final_best_accuracies = load_results('online_benchmark.p')
+
+plot_dataset_comparison_graph('Drain Accuracies',
+                              drain_benchmark_accuracies,
+                              final_best_accuracies)
