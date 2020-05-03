bf3230cde90c1021841a4a396d2ad945bd07f3be
unknown <justin_bunker@hotmail.com>
2020-05-03 15:25:51 -0400

    Update data_configs with assignment and template paths

--- src/data_config.py
+++ src/data_config.py
@@ -118,24 +118,24 @@ class DataConfigs:
     # Full Datasets
     Android_FULL = {
         'name': 'Android',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Android.csv',
         'unstructured_path': 'data/full/unstructured/android.log',
         'log_format': '<Date> <Time>  <Pid>  <Tid> <Level> <Component>: <Content>',
         'regex': [r'(/[\w-]+)+', r'([\w-]+\.){2,}[\w-]+',
                   r'\b(\-?\+?\d+)\b|\b0[Xx][a-fA-F\d]+\b|\b[a-fA-F\d]{4,}\b'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Android_templates.csv',
     }
     Apache_FULL = {
         'name': 'Apache',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Apache.csv',
         'unstructured_path': 'data/full/unstructured/apache.log',
         'log_format': '\[<Time>\] \[<Level>\] <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Apache_templates.csv',
     }
     BGL_FULL = {
         'name': 'BGL',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/BGL.csv',
         'unstructured_path': 'data/full/unstructured/bgl.log',
         'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
         'regex': [r'core\.\d+'],
@@ -143,90 +143,90 @@ class DataConfigs:
     }
     Hadoop_FULL = {
         'name': 'Hadoop',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Hadoop.csv',
         'unstructured_path': 'data/full/unstructured/hadoop.log',
         'log_format': '<Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Hadoop_templates.csv',
     }
     HDFS_FULL = {
         'name': 'HDFS',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/HDFS.csv',
         'unstructured_path': 'data/full/unstructured/hdfs.log',
         'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
         'regex': [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'],
-        'template_path': '',
+        'template_path': 'data/full/templates/HDFS_templates.csv',
     }
     HealthApp_FULL = {
         'name': 'HealthApp',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/HealthApp.csv',
         'unstructured_path': 'data/full/unstructured/healthapp.log',
         'log_format': '<Time>\|<Component>\|<Pid>\|<Content>',
         'regex': [],
-        'template_path': '',
+        'template_path': 'data/full/templates/HealthApp_templates.csv',
     }
     HPC_FULL = {
         'name': 'HPC',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/HPC.csv',
         'unstructured_path': 'data/full/unstructured/hpc.log',
         'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
         'regex': [r'=\d+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/HPC_templates.csv',
     }
     Linux_FULL = {
         'name': 'Linux',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Linux.csv',
         'unstructured_path': 'data/full/unstructured/linux.log',
         'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\d{2}:\d{2}:\d{2}'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Linux_templates.csv',
     }
     Mac_FULL = {
         'name': 'Mac',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Mac.csv',
         'unstructured_path': 'data/full/unstructured/mac.log',
         'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
         'regex': [r'([\w-]+\.){2,}[\w-]+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Mac_templates.csv',
     }
     OpenSSH_FULL = {
         'name': 'OpenSSH',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/OpenSSH.csv',
         'unstructured_path': 'data/full/unstructured/openssh.log',
         'log_format': '<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'([\w-]+\.){2,}[\w-]+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/OpenSSH_templates.csv',
     }
     OpenStack_FULL = {
         'name': 'OpenStack',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/OpenStack.csv',
         'unstructured_path': 'data/full/unstructured/openstack.log',
         'log_format': '<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>',
         'regex': [r'((\d+\.){3}\d+,?)+', r'/.+?\s', r'\d+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/OpenStack_templates.csv',
     }
     Proxifier_FULL = {
         'name': 'Proxifier',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Proxifier.csv',
         'unstructured_path': 'data/full/unstructured/proxifier.log',
         'log_format': '\[<Time>\] <Program> - <Content>',
         'regex': [r'<\d+\ssec', r'([\w-]+\.)+[\w-]+(:\d+)?',
                   r'\d{2}:\d{2}(:\d{2})*', r'[KGTM]B'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Proxifier_templates.csv',
     }
     Spark_FULL = {
         'name': 'Spark',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Spark.csv',
         'unstructured_path': 'data/full/unstructured/spark.log',
         'log_format': '<Date> <Time> <Level> <Component>: <Content>',
         'regex': [r'(\d+\.){3}\d+', r'\b[KGTM]?B\b', r'([\w-]+\.){2,}[\w-]+'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Spark_templates.csv',
     }
     Zookeeper_FULL = {
         'name': 'Zookeeper',
-        'assignments_path': '',
+        'assignments_path': 'data/full/assignments/Zookeeper.csv',
         'unstructured_path': 'data/full/unstructured/zookeeper.log',
         'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
         'regex': [r'(/|)(\d+\.){3}\d+(:\d+)?'],
-        'template_path': '',
+        'template_path': 'data/full/templates/Zookeeper_templates.csv',
     }
