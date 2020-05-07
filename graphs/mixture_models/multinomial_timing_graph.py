72eb1124157d3880886270ed39b1637ed5fe8c26
unknown <justin_bunker@hotmail.com>
2020-05-07 01:50:29 -0400

    Fix labeling of multinomial_timing_graph

--- graphs/mixture_models/multinomial_timing_graph.py
+++ graphs/mixture_models/multinomial_timing_graph.py
@@ -2,5 +2,5 @@ from global_utils import load_results
 
 results = load_results('multinomial_timing.p')
 
-print('Scipy implementation: {}'.format(results['custom_time']))
-print('Custom implementation: {}'.format(results['scipy_time']))
+print('Scipy implementation: {}'.format(results['scipy_time']))
+print('Custom implementation: {}'.format(results['custom_time']))
