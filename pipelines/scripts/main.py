39b3b10884f55d3851b6a21b081bb8778ce1e51c
unknown <justin_bunker@hotmail.com>
2020-05-09 02:19:59 -0400

    Update main pipeline script

--- pipelines/scripts/main.py
+++ pipelines/scripts/main.py
@@ -30,22 +30,27 @@ if __name__ == '__main__':
                 DataConfigs.Windows,
                 DataConfigs.Zookeeper,
             ],
-            'n_samples': 3,
+            'n_samples': 50,
             'label_count_values': [0, 20, 40, 60, 80, 100],
         },
         {
-            NAME: 'run_online_em_test',
+            NAME: 'run_online_em',
             FUNCTION: run_online_em,
-            'data_config': DataConfigs.BGL,
-            'n_sample': 3,
+            'data_config': DataConfigs.Apache,
+            'n_sample': 100,
             'training_sizes': [50, 100, 200, 300, 400, 500, 600, 700, 800, 900,
                                1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700,
                                1800, 1900, 2000],
         },
         {
-            NAME: 'run_online_benchmark_test',
+            NAME: 'run_online_benchmark_40_lab',
             FUNCTION: run_online_benchmark,
-            'n_labels': 20,
+            'n_labels': 40,
+        },
+        {
+            NAME: 'run_online_benchmark_80_lab',
+            FUNCTION: run_online_benchmark,
+            'n_labels': 80,
         }
     ]
 
