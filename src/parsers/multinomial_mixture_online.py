d41c936b619aa70a89b55e6f47a2319495d9f97a
unknown <justin_bunker@hotmail.com>
2020-05-23 19:21:14 -0400

    Fix minor bug in MultinomialMixtureOnline constraints enforcement

--- src/parsers/multinomial_mixture_online.py
+++ src/parsers/multinomial_mixture_online.py
@@ -163,17 +163,17 @@ class MultinomialMixtureOnline(LogParserOnline):
             r1 = self._get_responsibilities(c1)
             r2 = self._get_responsibilities(c2)
 
-            g1_first, g1_second = get_top_k_args(r1, 2)
-            g2_first, g2_second = get_top_k_args(r2, 2)
+            g1_first, = get_top_k_args(r1, 1)
+            g2_first, = get_top_k_args(r2, 1)
 
             if g1_first == g2_first:
                 continue
 
-            p1_first, p1_second = r1[g1_first], r1[g1_second]
-            p2_first, p2_second = r2[g2_first], r2[g2_second]
+            p1_first = r1[g1_first]
+            p2_first = r2[g2_first]
 
-            if p1_first < p2_second:
-                self._change_dominant_resp(c1, g1_first, g1_second)
+            if p1_first < p2_first:
+                self._change_dominant_resp(c1, g1_first, g2_first)
             else:
                 self._change_dominant_resp(c2, g2_first, g1_first)
 
@@ -192,15 +192,15 @@ class MultinomialMixtureOnline(LogParserOnline):
             if g1_first != g2_first:
                 continue
 
-            p1_first, p1_second = r1[g1_first], r1[g1_second]
-            p2_second, p2_second = r2[g2_first], r2[g2_second]
+            p1_first = r1[g1_first]
+            p2_first = r2[g2_first]
 
             g_empty = self._get_empty_cluster()
-            if g_empty != -1 and p1_first < p2_second:
+            if g_empty != -1 and p1_first < p2_first:
                 self._change_dominant_resp(c1, g1_first, g_empty)
-            elif g_empty != -1 and p1_first >= p2_second:
+            elif g_empty != -1 and p1_first >= p2_first:
                 self._change_dominant_resp(c2, g2_first, g_empty)
-            elif p1_first < p2_second:
+            elif p1_first < p2_first:
                 self._change_dominant_resp(c1, g1_first, g1_second)
             else:
                 self._change_dominant_resp(c2, g2_first, g2_second)
