61df2f2a863f62ad52a9e1b878d5b1f98797f0fa
unknown <justin_bunker@hotmail.com>
2020-05-31 16:06:39 -0400

    Update feedback_convergence_graph tight_layout and legend position

--- graphs/constraints_feedback/feedback_convergence_graph.py
+++ graphs/constraints_feedback/feedback_convergence_graph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from global_utils import load_results
 
 SUBPLOT_DIM = (4, 4)
-LEGEND_IDX = 13
+LEGEND_IDX = 16
 
 results = load_results('feedback_convergence.p')
 
@@ -13,7 +13,7 @@ for idx, name in enumerate(results, start=1):
     plt.title(name)
     plt.grid()
     plt.ylim((0, 1.05))
-    plt.tight_layout()
+    plt.tight_layout(0.1)
     imp_rate_results = results[name]['cem']
     for improvement_rate in imp_rate_results:
         acc_vals = imp_rate_results[improvement_rate]['acc']
