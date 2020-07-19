d6344b7949981f75c7c591789b4b255e7dd466c8
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-07-19 19:00:31 -0400

    Perform minor changes to coreset_evaluations_graph

--- graphs/coresets/coreset_evaluations_graph.py
+++ graphs/coresets/coreset_evaluations_graph.py
@@ -5,14 +5,14 @@ from src.data_config import DataConfigs
 SUBPLOT_DIM = (2, 4)
 
 data_configs = [
-    # DataConfigs.Android,
+    DataConfigs.Android,
     DataConfigs.Apache,
-    # DataConfigs.BGL,
-    # DataConfigs.Hadoop,
-    # DataConfigs.HDFS,
-    # DataConfigs.HealthApp,
-    # DataConfigs.HPC,
-    # DataConfigs.Linux,
+    DataConfigs.BGL,
+    DataConfigs.Hadoop,
+    DataConfigs.HDFS,
+    DataConfigs.HealthApp,
+    DataConfigs.HPC,
+    DataConfigs.Linux,
     # DataConfigs.Mac,
     # DataConfigs.OpenSSH,
     # DataConfigs.OpenStack,
@@ -28,17 +28,18 @@ means = []
 for idx, data_config in enumerate(data_configs, start=1):
     plt.subplot(*SUBPLOT_DIM, idx)
     name = data_config['name'].lower()
-    results = load_results('coreset_evaluations_{}_nmi_pos.p'.format(name))
+    results = load_results(
+        'coreset_evaluations_{}_nmi_pos.p'.format(name))
     score_samples_offline = results['score_samples_offline']
-    score_samples_coreset = results['score_samples_coreset']
     score_samples_online = results['score_samples_offline']
+    score_samples_coreset = results['score_samples_coreset']
     coreset_size = results['coreset_size']
 
     plt.title('{} ($n_c={}$)'.format(name, coreset_size))
     plt.boxplot([score_samples_offline,
-                 score_samples_coreset,
-                 score_samples_online],
-                labels=['offline', 'coreset', 'online'])
+                 score_samples_online,
+                 score_samples_coreset],
+                labels=['offline', 'online', 'coreset'])
     plt.grid()
 
 plt.show()
