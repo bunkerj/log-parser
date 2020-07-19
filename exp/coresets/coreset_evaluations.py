b8e2a9aa293f8d2ff3cd9a988220b44d2736fb37
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-07-19 01:35:01 -0400

    Update coreset_evaluations scripts

--- exp/coresets/coreset_evaluations.py
+++ exp/coresets/coreset_evaluations.py
@@ -1,6 +1,7 @@
 """
 Compare performance of offline EM with and without the coreset.
 """
+import numpy as np
 from copy import deepcopy
 from src.coresets.greedy_iterative_geodesic_ascent import \
     GreedyIterativeGeodesicAscent
@@ -20,42 +21,50 @@ def run_coreset_evaluations(proj_dim, subset_size, n_samples, data_config):
     evaluator = Evaluator(true_assignments)
     num_true_clusters = get_num_true_clusters(true_assignments)
 
+    mmo_pos_init = MultinomialMixtureOnline(tokenized_logs, num_true_clusters,
+                                            alpha=1.05,
+                                            beta=1.05)
+    mmo_pos_init.perform_online_batch_em(tokenized_logs)
+
+    cluster_pos = np.array(mmo_pos_init.t_c)
+    vocab_pos = np.array(mmo_pos_init.t_v)
+
     geo_ascent = GreedyIterativeGeodesicAscent(tokenized_logs,
+                                               # cluster_pos=cluster_pos,
+                                               # vocab_pos=vocab_pos,
                                                num_clusters=num_true_clusters)
     reduced_weights, reduced_set = geo_ascent.get_coreset(subset_size, proj_dim)
 
-    score_samples = []
-    coreset_score_samples = []
+    score_samples_offline = []
+    score_samples_coreset = []
+    score_samples_online = []
     for _ in range(n_samples):
-        mmo = MultinomialMixtureOnline(tokenized_logs,
-                                       num_true_clusters,
+        mmo = MultinomialMixtureOnline(tokenized_logs, num_true_clusters,
                                        alpha=1.05,
                                        beta=1.05)
         mmo_coreset = deepcopy(mmo)
-
-        mmo.perform_offline_em(tokenized_logs)
-        mmo_coreset.perform_offline_em(reduced_set, weights=reduced_weights)
-
-        mmo = MultinomialMixtureOnline(tokenized_logs,
-                                       num_true_clusters,
-                                       alpha=1.05,
-                                       beta=1.05)
-        mmo_coreset = deepcopy(mmo)
+        mmo_online = deepcopy(mmo)
 
         mmo.perform_offline_em(tokenized_logs)
         mmo_clusters = mmo.get_clusters(tokenized_logs)
-        score = evaluator.get_impurity(mmo_clusters, [])
+        score_offline = evaluator.get_nmi(mmo_clusters)
 
         mmo_coreset.perform_offline_em(reduced_set, weights=reduced_weights)
         mmo_coreset_clusters = mmo_coreset.get_clusters(tokenized_logs)
-        coreset_score = evaluator.get_impurity(mmo_coreset_clusters, [])
+        score_coreset = evaluator.get_nmi(mmo_coreset_clusters)
 
-        score_samples.append(score)
-        coreset_score_samples.append(coreset_score)
+        mmo_online.perform_online_batch_em(tokenized_logs)
+        mmo_online_clusters = mmo_online.get_clusters(tokenized_logs)
+        score_online = evaluator.get_nmi(mmo_online_clusters)
+
+        score_samples_offline.append(score_offline)
+        score_samples_coreset.append(score_coreset)
+        score_samples_online.append(score_online)
 
     return {
-        'score_samples': score_samples,
-        'coreset_score_samples': coreset_score_samples,
+        'score_samples_offline': score_samples_offline,
+        'score_samples_coreset': score_samples_coreset,
+        'score_samples_online': score_samples_online,
         'coreset_size': len(reduced_weights),
     }
 
@@ -89,5 +98,5 @@ if __name__ == '__main__':
         print(name)
         results = run_coreset_evaluations(proj_dim, subset_size, n_samples,
                                           data_config)
-        filename = 'coreset_evaluations_{}.p'.format(name)
+        filename = 'coreset_evaluations_{}_nmi_pos.p'.format(name)
         dump_results(filename, results)
--- graphs/coresets/coreset_evaluations_graph.py
+++ graphs/coresets/coreset_evaluations_graph.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
-from statistics import mean
 from global_utils import load_results
 from src.data_config import DataConfigs
 
 SUBPLOT_DIM = (2, 4)
 
 data_configs = [
-    DataConfigs.Android,
+    # DataConfigs.Android,
     DataConfigs.Apache,
-    DataConfigs.BGL,
-    DataConfigs.Hadoop,
-    DataConfigs.HDFS,
-    DataConfigs.HealthApp,
-    DataConfigs.HPC,
-    DataConfigs.Linux,
+    # DataConfigs.BGL,
+    # DataConfigs.Hadoop,
+    # DataConfigs.HDFS,
+    # DataConfigs.HealthApp,
+    # DataConfigs.HPC,
+    # DataConfigs.Linux,
     # DataConfigs.Mac,
     # DataConfigs.OpenSSH,
     # DataConfigs.OpenStack,
@@ -29,14 +28,17 @@ means = []
 for idx, data_config in enumerate(data_configs, start=1):
     plt.subplot(*SUBPLOT_DIM, idx)
     name = data_config['name'].lower()
-    results = load_results('coreset_evaluations_{}.p'.format(name))
-    score_samples = results['score_samples']
-    coreset_score_samples = results['coreset_score_samples']
+    results = load_results('coreset_evaluations_{}_nmi_pos.p'.format(name))
+    score_samples_offline = results['score_samples_offline']
+    score_samples_coreset = results['score_samples_coreset']
+    score_samples_online = results['score_samples_offline']
     coreset_size = results['coreset_size']
 
     plt.title('{} ($n_c={}$)'.format(name, coreset_size))
-    plt.boxplot([score_samples, coreset_score_samples],
-                labels=['score_mean', 'coreset_score_mean'])
+    plt.boxplot([score_samples_offline,
+                 score_samples_coreset,
+                 score_samples_online],
+                labels=['offline', 'coreset', 'online'])
     plt.grid()
 
 plt.show()
