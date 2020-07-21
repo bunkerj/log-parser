8a10f75654049fc308a766694747c31a5772d357
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-07-21 16:38:05 -0400

    Add corest_performance_vs_dim scripts

---
+++ exp/coresets/coreset_performance_vs_dim.py
@@ -0,0 +1,28 @@
+from global_utils import dump_results
+from src.data_config import DataConfigs
+from exp.coresets.utils import run_coreset_exp
+
+if __name__ == '__main__':
+    data_configs = [
+        DataConfigs.Android,
+        DataConfigs.Apache,
+        DataConfigs.BGL,
+        DataConfigs.Hadoop,
+        DataConfigs.HDFS,
+        DataConfigs.HealthApp,
+        DataConfigs.HPC,
+        DataConfigs.Linux,
+    ]
+
+    results = {}
+    for data_config in data_configs:
+        name = data_config['name'].lower()
+        results[name] = {}
+        for dim in range(10, 600, 50):
+            results[name][dim] = run_coreset_exp(proj_dim=dim,
+                                                 subset_size=50,
+                                                 n_samples=25,
+                                                 data_config=data_config)
+
+    filename = 'coreset_performance_vs_dim.p'
+    dump_results(filename, results)
---
+++ graphs/coresets/coreset_performance_vs_dim_graph.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from statistics import mean
+from global_utils import load_results
+
+SUBPLOT_DIM = (2, 4)
+
+results = load_results('coreset_performance_vs_dim.p')
+
+for idx, name in enumerate(results, start=1):
+    plt.subplot(*SUBPLOT_DIM, idx)
+
+    mean_scores_online = []
+    mean_scores_coreset = []
+
+    projection_dim = list(results[name].keys())
+    for dim in projection_dim:
+        score_samples_online = results[name][dim]['score_samples_online']
+        score_samples_coreset = results[name][dim]['score_samples_coreset']
+        mean_scores_online.append(mean(score_samples_online))
+        mean_scores_coreset.append(mean(score_samples_coreset))
+
+    plt.title(name)
+    plt.plot(projection_dim, mean_scores_online)
+    plt.plot(projection_dim, mean_scores_coreset)
+    plt.legend(['Mean Impurity Online', 'Mean Impurity Coreset'])
+    plt.grid()
+
+plt.show()
