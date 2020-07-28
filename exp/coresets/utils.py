80e9b2f89c83d8799a315f0e1cbf9e2de336147c
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-07-28 12:46:35 -0400

    Update coreset/utils so that it tracks offline EM and the coreset size

--- exp/coresets/utils.py
+++ exp/coresets/utils.py
@@ -21,11 +21,17 @@ def run_coreset_exp_mp(proj_dim, subset_size, n_samples, data_config):
         arguments = [(evaluator, num_clusters, proj_dim,
                       subset_size, tokenized_logs) for _ in range(n_samples)]
         mp_results = pool.starmap(run_single_coreset_exp, arguments)
-        score_samples_coreset, score_samples_online = list(zip(*mp_results))
+
+        score_samples_online, \
+        score_samples_offline, \
+        score_samples_coreset, \
+        coreset_sizes = list(zip(*mp_results))
 
     return {
         'score_samples_online': score_samples_online,
+        'score_samples_offline': score_samples_offline,
         'score_samples_coreset': score_samples_coreset,
+        'coreset_sizes': coreset_sizes,
     }
 
 
@@ -34,22 +40,32 @@ def run_single_coreset_exp(evaluator, num_clusters, proj_dim, subset_size,
     mmo_online = MultinomialMixtureOnline(tokenized_logs,
                                           num_clusters,
                                           is_classification=False,
+                                          epsilon=0.03,
                                           alpha=1.05,
                                           beta=1.05)
+    mmo_offline = deepcopy(mmo_online)
     mmo_coreset = deepcopy(mmo_online)
+
     mmo_online.perform_online_batch_em(tokenized_logs)
     cluster_pos = np.array(mmo_online.t_c)
     vocab_pos = np.array(mmo_online.t_v)
+
     geo_ascent = GreedyIterativeGeodesicAscent(tokenized_logs,
                                                cluster_pos=cluster_pos,
                                                vocab_pos=vocab_pos,
                                                num_clusters=num_clusters)
     reduced_weights, reduced_set = geo_ascent.get_coreset(subset_size,
                                                           proj_dim)
-    mmo_online_clusters = mmo_online.get_clusters(tokenized_logs)
-    score_online = evaluator.get_nmi(mmo_online_clusters)
+    coreset_size = len(reduced_weights)
+    mmo_offline.perform_offline_em(tokenized_logs)
     mmo_coreset.perform_offline_em(reduced_set, weights=reduced_weights)
+
+    mmo_online_clusters = mmo_online.get_clusters(tokenized_logs)
+    mmo_offline_clusters = mmo_offline.get_clusters(tokenized_logs)
     mmo_coreset_clusters = mmo_coreset.get_clusters(tokenized_logs)
+
+    score_online = evaluator.get_nmi(mmo_online_clusters)
+    score_offline = evaluator.get_nmi(mmo_offline_clusters)
     score_coreset = evaluator.get_nmi(mmo_coreset_clusters)
 
-    return score_coreset, score_online
+    return score_online, score_offline, score_coreset, coreset_size
