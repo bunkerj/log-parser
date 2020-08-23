2a54247c0a85d6773ce829ed66f9eb3f66584fd4
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-08-22 20:46:59 -0400

    Update feedback_vb_comparison so that it uses multiprocessing

--- exp/mixture_models/feedback_vb_comparison_mp.py
+++ exp/mixture_models/feedback_vb_comparison_mp.py
@@ -2,6 +2,7 @@
 Compare MultinomialMixtureVB scores with and without pairwise constraints for
 different datasets.
 """
+import multiprocessing as mp
 from global_utils import dump_results
 from src.data_config import DataConfigs
 from src.helpers.data_manager import DataManager
@@ -21,53 +22,57 @@ def get_mappings(cluster_templates, tokenized_logs):
     return new_cluster_templates
 
 
+def run_single_sample_exp(logs, true_assignments, num_clusters, n_label,
+                          evaluator, oracle, n_constraints):
+    log_labels = get_log_labels(true_assignments, n_label)
+
+    # Baseline
+    parser = MultinomialMixtureVB()
+    parser.fit(logs, num_clusters, log_labels=None,
+               constraints=None, epsilon=0.0001, max_iter=25)
+    c = parser.predict(logs)
+
+    # Labeled Model
+    parser_lab = MultinomialMixtureVB()
+    parser_lab.fit(logs, num_clusters, log_labels=log_labels,
+                   constraints=None, epsilon=0.0001, max_iter=25)
+    labeled_indices = parser_lab.get_labeled_indices()
+    c_lab = parser_lab.predict(logs)
+
+    # Labeled + Constrained Model
+    parser_lab_const = MultinomialMixtureVB()
+    W = oracle.get_constraints_matrix(
+        parsed_clusters=c_lab,
+        n_constraint_samples=n_constraints,
+        tokenized_logs=logs,
+        weight=1)
+    parser_lab_const.fit(logs, num_clusters, log_labels=None,
+                         constraints=W, epsilon=0.0001, max_iter=25)
+    c_lab_const = parser_lab_const.predict(logs)
+
+    # Calculate Scores
+    score = evaluator.get_impurity(c, labeled_indices)
+    score_lab = evaluator.get_impurity(c_lab, labeled_indices)
+    score_lab_const = evaluator.get_impurity(c_lab_const, labeled_indices)
+
+    return score, score_lab, score_lab_const
+
+
 def run_feedback_vb_comparison(n_label, n_constraints, n_samples, data_config):
     data_manager = DataManager(data_config)
-    tokenized_logs = data_manager.get_tokenized_logs()
+    logs = data_manager.get_tokenized_logs()
     true_assignments = data_manager.get_true_assignments()
     num_clusters = get_num_true_clusters(true_assignments)
     evaluator = Evaluator(true_assignments)
     oracle = Oracle(true_assignments)
 
-    scores_base = []
-    scores_lab = []
-    scores_lab_const = []
-
-    for _ in range(n_samples):
-        log_labels = get_log_labels(true_assignments, n_label)
-
-        # Baseline
-        parser = MultinomialMixtureVB()
-        parser.fit(tokenized_logs, num_clusters, log_labels=None,
-                   constraints=None, epsilon=0.0001, max_iter=25)
-        c = parser.predict(tokenized_logs)
-
-        # Labeled Model
-        parser_lab = MultinomialMixtureVB()
-        parser_lab.fit(tokenized_logs, num_clusters, log_labels=log_labels,
-                       constraints=None, epsilon=0.0001, max_iter=25)
-        labeled_indices = parser_lab.get_labeled_indices()
-        c_lab = parser_lab.predict(tokenized_logs)
-
-        # Labeled + Constrained Model
-        parser_lab_const = MultinomialMixtureVB()
-        W = oracle.get_constraints_matrix(
-            parsed_clusters=c_lab,
-            n_constraint_samples=n_constraints,
-            tokenized_logs=tokenized_logs,
-            weight=1)
-        parser_lab_const.fit(tokenized_logs, num_clusters, log_labels=None,
-                             constraints=W, epsilon=0.0001, max_iter=25)
-        c_lab_const = parser_lab_const.predict(tokenized_logs)
-
-        # Calculate Scores
-        score = evaluator.get_impurity(c, labeled_indices)
-        score_lab = evaluator.get_impurity(c_lab, labeled_indices)
-        score_lab_const = evaluator.get_impurity(c_lab_const, labeled_indices)
-
-        scores_base.append(score)
-        scores_lab.append(score_lab)
-        scores_lab_const.append(score_lab_const)
+    with mp.Pool(mp.cpu_count()) as pool:
+        arguments = [(logs, true_assignments, num_clusters, n_label,
+                      evaluator, oracle, n_constraints) for _ in
+                     range(n_samples)]
+        mp_results = pool.starmap(run_single_sample_exp, arguments)
+        scores_base, scores_lab, scores_lab_const \
+            = list(zip(*mp_results))
 
     return {
         'base': scores_base,
