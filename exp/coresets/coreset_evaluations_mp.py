037426c81f5c423b5791f5cad27780fbe3e42798
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-08-31 14:17:53 -0400

    Update coreset_evaluations_mp so that it uses MultinomialMixtureVB

--- exp/coresets/coreset_evaluations_mp.py
+++ exp/coresets/coreset_evaluations_mp.py
@@ -3,7 +3,6 @@ Compare performance of offline EM with and without the coreset.
 """
 import numpy as np
 import multiprocessing as mp
-from copy import deepcopy
 from src.coresets.greedy_iterative_geodesic_ascent import \
     GreedyIterativeGeodesicAscent
 from exp.mixture_models.utils import get_num_true_clusters
@@ -12,6 +11,7 @@ from src.helpers.data_manager import DataManager
 from src.data_config import DataConfigs
 from src.helpers.evaluator import Evaluator
 from src.parsers.multinomial_mixture_online import MultinomialMixtureOnline
+from src.parsers.multinomial_mixture_vb import MultinomialMixtureVB
 
 
 def run_coreset_evaluations_mp(proj_dim, subset_size, n_samples, data_config):
@@ -39,11 +39,9 @@ def run_coreset_evaluations_mp(proj_dim, subset_size, n_samples, data_config):
     reduced_weights, reduced_set = geo_ascent.get_coreset(subset_size, proj_dim)
 
     with mp.Pool(mp.cpu_count()) as pool:
-        arguments = [(evaluator,
-                      num_clusters,
-                      reduced_set,
-                      reduced_weights,
-                      tokenized_logs) for _ in range(n_samples)]
+        argument_list = (evaluator, num_clusters, reduced_set,
+                         reduced_weights, tokenized_logs)
+        arguments = [argument_list for _ in range(n_samples)]
         mp_results = pool.starmap(run_single_coreset_evaluation, arguments)
         score_samples_coreset, score_samples_offline, score_samples_online \
             = list(zip(*mp_results))
@@ -57,27 +55,25 @@ def run_coreset_evaluations_mp(proj_dim, subset_size, n_samples, data_config):
 
 
 def run_single_coreset_evaluation(evaluator, num_clusters, reduced_set,
-                                  reduced_weights,
-                                  tokenized_logs):
-    mmo = MultinomialMixtureOnline(tokenized_logs,
-                                   num_clusters,
-                                   is_classification=False,
-                                   alpha=1.05,
-                                   beta=1.05)
-    mmo_coreset = deepcopy(mmo)
-    mmo_online = deepcopy(mmo)
+                                  reduced_weights, tokenized_logs):
+    mm = MultinomialMixtureVB()
+    mm.fit(tokenized_logs, num_clusters)
+    mm_clusters = mm.predict(tokenized_logs)
+    score_offline = evaluator.get_impurity(mm_clusters, [])
 
-    mmo.perform_offline_em(tokenized_logs)
-    mmo_clusters = mmo.get_clusters(tokenized_logs)
-    score_offline = evaluator.get_nmi(mmo_clusters)
+    mm_coreset = MultinomialMixtureVB()
+    mm_coreset.fit(reduced_set, num_clusters, cs_weights=reduced_weights)
+    mm_coreset_clusters = mm_coreset.predict(tokenized_logs)
+    score_coreset = evaluator.get_impurity(mm_coreset_clusters, [])
 
-    mmo_coreset.perform_offline_em(reduced_set, weights=reduced_weights)
-    mmo_coreset_clusters = mmo_coreset.get_clusters(tokenized_logs)
-    score_coreset = evaluator.get_nmi(mmo_coreset_clusters)
-
+    mmo_online = MultinomialMixtureOnline(tokenized_logs,
+                                          num_clusters,
+                                          is_classification=False,
+                                          alpha=1.05,
+                                          beta=1.05)
     mmo_online.perform_online_batch_em(tokenized_logs)
     mmo_online_clusters = mmo_online.get_clusters(tokenized_logs)
-    score_online = evaluator.get_nmi(mmo_online_clusters)
+    score_online = evaluator.get_impurity(mmo_online_clusters, [])
 
     return score_coreset, score_offline, score_online
 
@@ -85,7 +81,7 @@ def run_single_coreset_evaluation(evaluator, num_clusters, reduced_set,
 if __name__ == '__main__':
     proj_dim = 500
     subset_size = 50
-    n_samples = 10
+    n_samples = 1000
 
     data_configs = [
         DataConfigs.Android,
