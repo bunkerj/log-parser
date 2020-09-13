3fa9f87fc7038f8012c80649d37be2ce6ec1319c
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-09-13 19:24:31 -0400

    Update get_log_labels to sample with replacement

--- exp/mixture_models/utils.py
+++ exp/mixture_models/utils.py
@@ -1,9 +1,9 @@
-from random import sample
+from random import choices
 
 
 def get_log_labels(true_assignments, num_of_labels):
     log_labels = {}
-    labeled_indices = sample(range(len(true_assignments)), k=num_of_labels)
+    labeled_indices = choices(range(len(true_assignments)), k=num_of_labels)
     for log_idx in labeled_indices:
         cluster = true_assignments[log_idx][-1]
         if cluster not in log_labels:
