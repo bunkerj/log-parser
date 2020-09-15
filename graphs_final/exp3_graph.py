1178dc2c834dea8dcfd173ed3f9a8ad138ce9ac3
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-09-15 11:20:19 -0400

    Update final exp3_graph so that it averages across samples

--- graphs_final/exp3_graph.py
+++ graphs_final/exp3_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-
 from global_utils import load_results
+from graphs_final.utils import get_sample_avg
 
 results = load_results('exp3_results.p')
 
@@ -8,7 +8,8 @@ N = len(results['label_counts'])
 drain_scores = N * [results['drain_score']]
 
 plt.subplot(1, 2, 1)
-plt.plot(results['label_counts'], results['label_score_samples'])
+avg_label_scores = get_sample_avg(results['label_score_samples'])
+plt.plot(results['label_counts'], avg_label_scores)
 plt.plot(results['label_counts'], drain_scores)
 plt.legend(['Multinomial VB', 'Drain'])
 plt.ylabel('AMI')
@@ -16,7 +17,8 @@ plt.xlabel('Label Counts')
 plt.grid()
 
 plt.subplot(1, 2, 2)
-plt.plot(results['constraint_counts'], results['constraint_score_samples'])
+avg_constraint_scores = get_sample_avg(results['constraint_score_samples'])
+plt.plot(results['constraint_counts'], avg_constraint_scores)
 plt.plot(results['label_counts'], drain_scores)
 plt.ylabel('AMI')
 plt.xlabel('Constraint Counts')
