1ac7e57eca2874c411cad15bf2dbdfee40a39343
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-09-15 12:58:18 -0400

    Add n_constraints variable to multinomial_vb single exp

--- exp/single_runs/multinomial_vb.py
+++ exp/single_runs/multinomial_vb.py
@@ -7,7 +7,7 @@ from src.helpers.oracle import Oracle
 from src.parsers.multinomial_mixture_vb import MultinomialMixtureVB
 
 
-def run_multinomial_vb(data_config, subset_size, n_labels):
+def run_multinomial_vb(data_config, subset_size, n_labels, n_constraints):
     data_manager = DataManager(data_config)
     logs, true_assignments = get_log_sample(data_manager, subset_size)
 
@@ -23,7 +23,7 @@ def run_multinomial_vb(data_config, subset_size, n_labels):
 
     W = oracle.get_constraints_matrix(
         parsed_clusters=clustering,
-        n_constraint_samples=n_labels,
+        n_constraint_samples=n_constraints,
         tokenized_logs=logs,
         weight=1)
     mm.fit(logs, n_clusters, log_labels=log_labels, p_weights=W)
@@ -35,5 +35,6 @@ if __name__ == '__main__':
     data_config = DataConfigs.BGL_FULL_FINAL
     subset_size = 50000
     n_labels = 5000
+    n_constraints = 5000
 
-    run_multinomial_vb(data_config, subset_size, n_labels)
+    run_multinomial_vb(data_config, subset_size, n_labels, n_constraints)
