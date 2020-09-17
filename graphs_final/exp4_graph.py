a751935d07a35141cc11fc42dc0073bdbe74b6fd
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-09-17 16:03:21 -0400

    Update final exp4 plot so that the random label curve is not an average constant

--- graphs_final/exp4_graph.py
+++ graphs_final/exp4_graph.py
@@ -6,10 +6,9 @@ results = load_results('exp4_results.p')
 
 N = len(results['cs_size'])
 avg_rand_label_scores = get_sample_avg(results['rand_label_ami_samples'])
-full_avg_rand_label_score = N * get_sample_avg([avg_rand_label_scores])
 avg_cs_label_scores = get_sample_avg(results['cs_label_ami_samples'])
 
-plt.plot(results['cs_size'], full_avg_rand_label_score)
+plt.plot(results['cs_size'], avg_rand_label_scores)
 plt.plot(results['cs_size'], avg_cs_label_scores)
 plt.legend(['Random', 'Coreset'])
 plt.xlabel('Coreset Size')
