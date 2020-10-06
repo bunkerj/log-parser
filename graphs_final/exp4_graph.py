4238b09c142eabd433355fb7f0596e1bdd6c3c77
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-10-06 17:04:52 -0400

    Update exp4 graph script so that it adds a least squares polynomial fit

--- graphs_final/exp4_graph.py
+++ graphs_final/exp4_graph.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from global_utils import load_results
 from graphs_final.utils import get_sample_avg
@@ -5,11 +6,17 @@ from graphs_final.utils import get_sample_avg
 results = load_results('exp4_results.p')
 
 N = len(results['cs_size'])
+cs_sizes_arr = np.array(results['cs_size'])
 avg_rand_label_scores = get_sample_avg(results['rand_label_ami_samples'])
 avg_cs_label_scores = get_sample_avg(results['cs_label_ami_samples'])
 
-plt.scatter(results['cs_size'], avg_rand_label_scores)
-plt.scatter(results['cs_size'], avg_cs_label_scores)
+m_rand, b_rand = np.polyfit(cs_sizes_arr, avg_rand_label_scores, 1)
+m_cs, b_cs = np.polyfit(cs_sizes_arr, avg_cs_label_scores, 1)
+
+plt.scatter(cs_sizes_arr, avg_rand_label_scores)
+plt.scatter(cs_sizes_arr, avg_cs_label_scores)
+plt.plot(cs_sizes_arr, m_rand * cs_sizes_arr + b_rand)
+plt.plot(cs_sizes_arr, m_cs * cs_sizes_arr + b_cs)
 plt.legend(['Random', 'Coreset'])
 plt.xlabel('Coreset Size')
 plt.ylabel('AMI')
