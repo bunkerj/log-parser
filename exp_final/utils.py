5758212d69c0c46d10b47e54b45f311a82300843
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-10-21 13:53:02 -0400

    Add get_constraint_strings()

--- exp_final/utils.py
+++ exp_final/utils.py
@@ -34,11 +34,27 @@ def get_coreset(logs, n_clusters, subset_size, proj_dim):
     return geo_ascent.get_coreset(subset_size, proj_dim)
 
 
+def get_log_str(logs, idx):
+    return ' '.join(logs[idx])
+
+
 def get_log_strings(logs, c_base):
     c_base_strings = {}
     for k in c_base:
         c_base_strings[k] = []
         for idx in c_base[k]:
-            log_str = ' '.join(logs[idx])
+            log_str = get_log_str(logs, idx)
             c_base_strings[k].append(log_str)
     return c_base_strings
+
+
+def get_constraint_strings(logs, constraints):
+    constraint_strings = {}
+    for k in constraints:
+        constraint_strings[k] = []
+        for link in constraints[k]:
+            idx1, idx2 = link
+            log_str1 = get_log_str(logs, idx1)
+            log_str2 = get_log_str(logs, idx2)
+            constraint_strings[k].append((log_str1, log_str2))
+    return constraint_strings
