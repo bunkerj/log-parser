ee20ba53c641f60c0622b7361152f32fd6a561dc
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-10-27 20:54:30 -0400

    Add n argument to plot_mean_with_std()

--- graphs_final/utils.py
+++ graphs_final/utils.py
@@ -15,11 +15,11 @@ def get_sample_std(samples):
     return get_sample_statistic(samples, stdev)
 
 
-def plot_mean_with_std(x_axis, mu, s, color, label):
+def plot_mean_with_std(x_axis, mu, s, n, color, label):
     mu_arr = np.array(mu)
     s_arr = np.array(s)
     plt.plot(x_axis, mu_arr, lw=2, label=label, color=color)
-    s_norm = s_arr / np.sqrt(len(x_axis))
+    s_norm = s_arr / np.sqrt(n)
     ub = mu_arr + s_norm
     lb = mu_arr - s_norm
     plt.fill_between(x_axis, lb, ub, facecolor=color, alpha=0.5)
