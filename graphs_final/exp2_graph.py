efba8e0e500f998077be292ba1eccbf0db2526b5
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-10-30 08:05:19 -0400

    Update final exp2_graph so that it is easier to swap metrics

--- graphs_final/exp2_graph.py
+++ graphs_final/exp2_graph.py
@@ -4,16 +4,21 @@ from graphs_final.utils import get_sample_avg, get_sample_std, \
     plot_mean_with_std
 from statistics import mean, stdev
 
+METRIC = 'ami'
+
 results = load_results('exp2_results.p')
 
 
 def plot_metric(feature, title, ylabel, xlabel):
+    score_cs_key = '{}_cs_samples'.format(METRIC)
+    score_base_key = '{}_base_samples'.format(METRIC)
     varying_sizes = results[feature]['varying_sizes']
-    avg_scores_cs = get_sample_avg(results[feature]['ami_cs_samples'])
-    std_scores_cs = get_sample_std(results[feature]['ami_cs_samples'])
+
+    avg_scores_cs = get_sample_avg(results[feature][score_cs_key])
+    std_scores_cs = get_sample_std(results[feature][score_cs_key])
 
     base_samples = []
-    for samples in results[feature]['ami_base_samples']:
+    for samples in results[feature][score_base_key]:
         base_samples.extend(samples)
 
     mean_base = mean(base_samples)
@@ -23,7 +28,7 @@ def plot_metric(feature, title, ylabel, xlabel):
     std_base_vector = [std_base] * len(varying_sizes)
 
     n_base = len(base_samples)
-    n_cs = len(results[feature]['ami_cs_samples'][0])
+    n_cs = len(results[feature][score_cs_key][0])
 
     plot_mean_with_std(varying_sizes, mean_base_vector, std_base_vector,
                        n_base, 'blue', 'Base')
@@ -37,8 +42,8 @@ def plot_metric(feature, title, ylabel, xlabel):
     plt.grid()
 
 
-plot_metric('cs_ub', 'Performance vs UB Size', 'AMI', 'Coreset UB Size')
-
-plt.subplots_adjust(left=0.1, bottom=0.06, right=0.99,
+plot_metric('cs_ub', 'Performance vs UB Size',
+            METRIC.upper(), 'Coreset UB Size')
+plt.subplots_adjust(left=0.1, bottom=0.1, right=0.99,
                     top=0.95, wspace=0.3, hspace=0.5)
 plt.show()
