7ace132b3dcf557afa7e157da78b11bdc76795d4
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-10-30 08:06:08 -0400

    Increase default n_samples to 1000 in final exp1_4

--- exp_final/exp1_4_mp.py
+++ exp_final/exp1_4_mp.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 
     n_labels = 50
     n_consts = 100
-    n_samples = 25
+    n_samples = 1000
 
     filename = 'exp1_4_results.p'
     results = run_exp1_4_full(data_configs, n_labels, n_consts, n_samples)
