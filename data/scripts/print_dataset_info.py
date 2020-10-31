0c3a6e7d65428e580bee648766520d71b5260080
Justin Bunker <8287501+bunkerj@users.noreply.github.com>
2020-10-31 13:47:28 -0400

    Add additional characteristics in print_dataset_info

--- data/scripts/print_dataset_info.py
+++ data/scripts/print_dataset_info.py
@@ -1,7 +1,9 @@
+from sklearn import metrics
+from statistics import mean, stdev
 from global_utils import get_num_true_clusters
 from src.data_config import DataConfigs
 from src.helpers.data_manager import DataManager
-from src.utils import get_vocabulary_indices
+from src.utils import get_vocabulary_indices, get_token_counts_batch
 
 data_configs = [
     DataConfigs.Android,
@@ -20,26 +22,57 @@ data_configs = [
     DataConfigs.Thunderbird,
     DataConfigs.Windows,
     DataConfigs.Zookeeper,
-    DataConfigs.BGL_FULL_FINAL,
 ]
 
 for data_config in data_configs:
     name = data_config['name']
     data_manager = DataManager(data_config)
     logs = data_manager.get_tokenized_logs()
+    logs_raw = data_manager.get_tokenized_raw_logs()
     n_logs = len(logs)
 
     true_assignments = data_manager.get_true_assignments()
     n_clusters = get_num_true_clusters(true_assignments)
+    labels = [ta[-1] for ta in true_assignments]
 
     event_set = set([' '.join(log) for log in logs])
     n_unique_logs = len(event_set)
 
     v_indices = get_vocabulary_indices(logs)
+    v_indices_raw = get_vocabulary_indices(logs_raw)
     n_vocab = len(v_indices)
+    n_vocab_raw = len(v_indices_raw)
 
-    print(name)
-    print('Number of clusters: {}'.format(n_clusters))
-    print('Number of unique logs: {}'.format(n_unique_logs))
-    print('Number of unique tokens: {}'.format(n_vocab))
-    print('Number of logs: {}\n'.format(n_logs))
+    C = get_token_counts_batch(logs, v_indices)
+    C_raw = get_token_counts_batch(logs_raw, v_indices_raw)
+
+    silhouette = metrics.silhouette_score(C, labels)
+    silhouette_raw = metrics.silhouette_score(C_raw, labels)
+    db = metrics.davies_bouldin_score(C, labels)
+    db_raw = metrics.davies_bouldin_score(C_raw, labels)
+
+    log_lengths = [len(log) for log in logs]
+    log_raw_lengths = [len(log) for log in logs_raw]
+
+    avg_tokens_per_log_cont = mean(log_lengths)
+    avg_tokens_per_log_raw = mean(log_raw_lengths)
+    std_tokens_per_log_cont = stdev(log_lengths)
+    std_tokens_per_log_raw = stdev(log_raw_lengths)
+    raw_vs_cont_ratio = avg_tokens_per_log_raw / avg_tokens_per_log_cont
+
+    print('------ {} ------'.format(name))
+    print('n_clusters: {}'.format(n_clusters))
+    print('n_unique_logs: {}'.format(n_unique_logs))
+    print('n_vocab_raw: {}'.format(n_vocab_raw))
+    print('n_vocab: {}'.format(n_vocab))
+    print('n_logs: {}'.format(n_logs))
+    print('avg_tokens_per_log_raw: {}'.format(avg_tokens_per_log_raw))
+    print('avg_tokens_per_log_cont: {}'.format(avg_tokens_per_log_cont))
+    print('stdev_tok_per_log_raw: {:.4f}'.format(std_tokens_per_log_raw))
+    print('stdev_tok_per_log_cont: {:.4f}'.format(std_tokens_per_log_cont))
+    print('raw_vs_cont_ratio: {:.4f}'.format(raw_vs_cont_ratio))
+    print('silhouette: {:.4f}'.format(silhouette))
+    print('silhouette_raw: {:.4f}'.format(silhouette_raw))
+    print('db: {:.4f}'.format(db))
+    print('db_raw: {:.4f}'.format(db_raw))
+    print()
